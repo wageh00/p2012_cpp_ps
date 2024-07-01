84677737d90302f5b30e6b46ba3c13806fa79787
ahmed718 <ahmedashrafashrafazep@gmail.com>
2024-07-01 22:47:58 +0300

    problem 2 in c++

---
+++ cpp/ps0002/ps0002cpp.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   // int,short,double,unsigned,float,char;
+    
+short medication_alternatives=4;
+short residents_architecture=14;
+short group_team=12;
+float price_baked_goods=7.5;
+char first_letter ='A';
+double number_births=1430670000;
+float 5_divide_by7=0.7142;
+float electricity_bill=533.5;
+unsigned age_people_sec=90*365.25*24*60*60;
+
+    return 0;
+}
