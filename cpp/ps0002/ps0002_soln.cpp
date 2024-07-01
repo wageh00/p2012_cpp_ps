2dcd038637424d95237d55d2c64b6e47838a5d8c
Mennatalahtaha <167816045+Mennatalahtaha@users.noreply.github.com>
2024-07-02 02:09:23 +0300

    problem 2

---
+++ cpp/ps0002/ps0002_soln.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{/* Give examples for variables from real life and consider that:
+    - variables should be named in a way that makes sense
+    - variables should has a suitable data type
+    - variables should be initialized with a suitable value
+    - example:
+        int family_members = 8;
+*/
+   unsigned short num_residents=40;
+   unsigned short  medecine_alternatives=5;
+    unsigned short  team_members=8;
+     float  price_bakeryitem=12.5;
+      char firstletterofname='M';
+      unsigned long numBirths =5000000000;
+        double devisionresult=123.1243333333;
+      double electricitybill=2000.2345;
+      unsigned long ageInSeconds=2345674354654657;
+    return 0;
+}
