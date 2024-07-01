430c648d170590f097c1e8792978835ba8228b48
Mohamed Ashraf <107517330+Zero028@users.noreply.github.com>
2024-07-01 19:32:29 +0300

    Add files via upload

---
+++ cpp/ps0003/ps0003_sol.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    std::cin >> x;
+    if(x%2==0)
+        std::cout << "Number is even";
+        else
+        std::cout << "Number is odd";
+
+    return 0;
+}
