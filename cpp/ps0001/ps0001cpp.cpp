4aa76edeb155aa914976979ac9cf39b2f048d2d5
ahmed718 <ahmedashrafashrafazep@gmail.com>
2024-07-01 22:47:12 +0300

    problem 1 in c++

---
+++ cpp/ps0001/ps0001cpp.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a,b,c,result;
+    cout <<"enter three numbers to add \n";
+    cin>>a>>b>>c;
+    result=a+b+c;
+    cout<<"the sum of a , b and c =" << result ;
+ 
+    return 0;
+}
