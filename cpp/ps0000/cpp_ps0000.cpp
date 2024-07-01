64c55529aa5b2cb8cf1929e3d7b8e6970e24bf4d
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-01 21:41:39 +0300

    Update cpp_ps0000.cpp

--- cpp/ps0000/cpp_ps0000.cpp
+++ cpp/ps0000/cpp_ps0000.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <iostream> // 2/2
 
-int main(void)
+int main(void) // 1/1
 {
-  cout << "Hello, World!\n";
-  return 0;
+  cout << "Hello, World!\n"; // 4/4
+  return 0; // 1/1
 }
 // great work 😎 10/10 ⭐
