d5c2ba827ff4f8c769a85983de56f4f36e8774b3
Mennatalahtaha <167816045+Mennatalahtaha@users.noreply.github.com>
2024-07-02 00:01:11 +0300

    problem 1 in c++

---
+++ cpp/ps0001/ps0001_soln.cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{/* help your brother solve summation problems
+    155
+    940
+   +168
+  --------
+   1263
+*/
+int n1,n2,n3;
+cin>>n1;
+cin>>n2;
+cin>>n3;
+    cout << n1+n2+n3<<"\n";
+    return 0;
+}
