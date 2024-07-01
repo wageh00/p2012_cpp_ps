be9a2abec742406a4dd554cb222ad536a5116018
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-01 23:14:33 +0300

    Update ps0001cpp.cpp

--- cpp/ps0001/ps0001cpp.cpp
+++ cpp/ps0001/ps0001cpp.cpp
@@ -1,21 +1,14 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 using namespace std;
 int main()
 {
-    int a,b,c,result;
+    int a,b,c,result; // 1/2
     cout <<"enter three numbers to add \n";
-    cin>>a>>b>>c;
+    cin>>a>>b>>c; // 2/2
     result=a+b+c;
-    cout<<"the sum of a , b and c =" << result ;
+    cout<<"the sum of a , b and c =" << result ; // 2/2
  
     return 0;
 }
+// great work 😎 8/10 ⭐
