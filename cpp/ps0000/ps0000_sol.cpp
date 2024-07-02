c644fc8e9db7fe19b8fbd794e826fc09338b88c6
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-02 22:22:14 +0300

    problem 0 marked

--- cpp/ps0000/ps0000_sol.cpp
+++ cpp/ps0000/ps0000_sol.cpp
@@ -1,18 +1,21 @@
 // Replace @ with # when you include a library
 // There's no .h in iostream
 // Library can't be between double quotes
-#include <iostream>
+#include <iostream> // 2/2
 
 // main must be written ib small letters not capital
-int main(void)
+int main(void) // 1/1
 {
     // You must put std:: before cout
     // What comes after std::cout is << not >>
     // Remove /n since it's not wanted and we only want one line
     // Each line of code must end with a semicolon
 
-    std::cout << "Hello, World!";
+    std::cout << "Hello, World!" << std::endl; // 3/4
 
-  // You must return 0 in main function
-  return 0;
+    // You must return 0 in main function
+    return 0; // 1/1
 }
+// great work 😎 10/10 ⭐
+
+// +1 bonus mark
