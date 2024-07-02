dd2b32cc04712f4e94e87a9bf86f1238f037db3c
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-02 18:34:35 +0300

    problem 1 marked

--- cpp/ps0001/ps0001cpp.cpp
+++ cpp/ps0001/ps0001cpp.cpp
@@ -1,21 +1,17 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 using namespace std;
 int main()
 {
-    int a,b,c,result;
-    cout <<"enter three numbers to add \n";
-    cin>>a>>b>>c;
-    result=a+b+c;
-    cout<<"the sum of a , b and c =" << result ;
+    // prompting users for 3 numbers  1/2
+    unsigned short a, b, c, result;
+    cout << "enter three numbers to add \n";
+    cin >> a >> b >> c;
+
+    // outputting the result of the summation process 3/3
+    result = a + b + c;
+    cout << "the sum of a , b and c =" << result ;
  
     return 0;
 }
+// great work 😎 8/10 ⭐
