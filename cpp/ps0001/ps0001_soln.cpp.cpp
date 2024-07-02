791a2e2bd3feb4ff2139bba865f74a2a5b7495dd
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-02 19:45:09 +0300

    problem 1 marked

--- cpp/ps0001/ps0001_soln.cpp.cpp
+++ cpp/ps0001/ps0001_soln.cpp.cpp
@@ -3,17 +3,15 @@
 using namespace std;
 
 int main()
-{/* help your brother solve summation problems
-    155
-    940
-   +168
-  --------
-   1263
-*/
-int n1,n2,n3;
-cin>>n1;
-cin>>n2;
-cin>>n3;
-    cout << n1+n2+n3<<"\n";
+{
+    // prompting user for 3 numbers to add 1/2
+    unsigned short n1, n2, n3;
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+    
+    // outputting the result of the summation process 2/3
+    cout << n1 + n2 + n3 << "\n";
     return 0;
 }
+// good work 👍 7/10
