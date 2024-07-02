19517f2bb8364441cc469722ae108d6f669e463c
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-02 20:11:50 +0300

    problem 1 marked

--- cpp/ps0001/ps0001_sol.cpp
+++ cpp/ps0001/ps0001_sol.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main()
 {
-    int x,y,z;
-    cin >> x >> y >> z;
-    int sum=x+y+z;
+    // promptiung user for 3 numbers to add 1/2
+    unsigned short num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+
+    // outputting the result of the summation process 3/3
+    unsigned short sum = num1 + num2 + num3;
     cout << "---------" << endl;
     cout << sum;
     return 0;
 }
+// great work 😎 8/10 ⭐
