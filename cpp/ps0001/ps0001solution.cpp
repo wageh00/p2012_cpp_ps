9e49a1d10f6cadea036f4aed823f5d734efd537d
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-02 18:15:33 +0300

    problem 1 marked

--- cpp/ps0001/ps0001solution.cpp
+++ cpp/ps0001/ps0001solution.cpp
@@ -1,15 +1,15 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
+	// prompting user for 3 numbers to add
 	unsigned short num1, num2, num3;
-
 	cin >> num1 >> num2 >> num3;
-	cout << "--------" << "\n" << num1 + num2 + num3;
-	
+
+	// outputting the result of the summation process
+	cout << "--------\n" << num1 + num2 + num3;
 	
 	return 0;
 }
-
+// great work 😎 9/10 ⭐
