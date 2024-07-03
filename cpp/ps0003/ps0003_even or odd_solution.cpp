e26dbc975bd9da6fb7727a7a3c71ba3d3ed4a145
Sarah <149259734+SaraMaher5@users.noreply.github.com>
2024-07-03 10:48:50 +0300

    Add files via upload

---
+++ cpp/ps0003/ps0003_even or odd_solution.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout << " Enter your number: ";
+	cin >> num;
+
+	if (num % 2 == 0)
+		cout << "Number is even";
+	else
+		cout << "Number is odd";
+
+	return 0;
+}
+
