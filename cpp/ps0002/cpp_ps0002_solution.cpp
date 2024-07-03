d866d0ef5178ee901009656a5f8fad0e04556ef7
ganna960 <ge2019916@gmail.com>
2024-07-03 12:42:39 +0300

    problem 2 in c++

---
+++ cpp/ps0002/cpp_ps0002_solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int population = 55;
+	short alternative = 3;
+	int members = 8;
+	float price = 5.5;
+	char letter = 'h';
+	double birth = 700000;
+	float div = 2.33;
+	float expenses = 1230.75;
+	double age = 10521000;
+	return 0;
+
+
+}
