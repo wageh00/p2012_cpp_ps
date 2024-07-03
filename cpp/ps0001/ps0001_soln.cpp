ed8cf63aa80f769502f6fa2d392fd9c5336b3732
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-03 10:03:49 +0300

    problem 1 marked

---
+++ cpp/ps0001/ps0001_soln.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // prompting user for 3 numbers to add 2/2
+    unsigned short a,b,c,t;
+    std::cin >> a >> b >> c;
+
+    // outputting the result of the summation process 2/3
+    t = (a + b + c);
+    std::cout << t;
+    return 0;
+}
+// good work 👍 6/10
