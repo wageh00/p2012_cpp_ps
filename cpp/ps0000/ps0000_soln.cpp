4c41f5314a4cf657ee7adad9d32e9cc2f2ef79dc
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-03 09:54:45 +0300

    problem 0 marked

---
+++ cpp/ps0000/ps0000_soln.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // 2/2
+//we should write # not @ and the library should be <iostream> not <"iostream.h">
+
+int main() // 1/1
+{
+    std::cout << "Hello, World!\n"; // 4/4
+    // you should write std:: before cout  and there is erorr in syntax such as ,we should write << not >> and \n not /n
+   /*In C++, we use Cout << “ Text To be Displayed on The screen “;
+     To use Cout << , we must use
+         #include <iostream>;
+     using namespace std;*/
+  
+    return 0; // 1/1
+    // we write 0 not 1
+}
+// great work 😎 8/10 ⭐
+
+// +1 bonus mark
