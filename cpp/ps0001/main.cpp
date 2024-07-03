7457576bad5bc07fb68996bc5296d8d123e62a99
Mahmoodelsayed <mahmoodelsayed77@gmail.com>
2024-07-03 09:47:03 +0300

    Add files via upload

---
+++ cpp/ps0001/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int a;
+cout <<"enter value of a "<<endl;
+cin >> a ;
+int b;
+cout <<"enter value of b "<<endl;
+cin >> b ;
+int c;
+cout <<"enter value of c "<<endl;
+cin >> c ;
+int sum= a+b+c;
+cout <<"sum= "<< sum<<endl;
+return 0;
+}
