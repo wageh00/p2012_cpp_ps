bf2e8d4ebb7706e9ded8535f5f8afed65d4139fc
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-03 08:12:36 +0300

    problem 2 marked

--- cpp/ps0002/ps0002_soln.cpp
+++ cpp/ps0002/ps0002_soln.cpp
@@ -3,21 +3,17 @@
 using namespace std;
 
 int main()
-{/* Give examples for variables from real life and consider that:
-    - variables should be named in a way that makes sense
-    - variables should has a suitable data type
-    - variables should be initialized with a suitable value
-    - example:
-        int family_members = 8;
-*/
-   unsigned short num_residents=40;
-   unsigned short  medecine_alternatives=5;
-    unsigned short  team_members=8;
-     float  price_bakeryitem=12.5;
-      char firstletterofname='M';
-      unsigned long numBirths =5000000000;
-        double devisionresult=123.1243333333;
-      double electricitybill=2000.2345;
-      unsigned long ageInSeconds=2345674354654657;
+{
+    unsigned short num_residents = 40; // 0.75/0.75
+    unsigned short medecine_alternatives = 5; // 0.75/0.75
+    unsigned short team_members = 8; // 0.75/0.75
+    float price_bakeryitem = 12.5; // 0.75/0.75
+    char first_letter_of_name = 'M'; // 0.75/0.75
+    unsigned int numBirths = 5000000000; // 0.75/0.75
+    long double devisionresult = 123.1243333333; // 0.5/0.75
+    double electricitybill = 2000.2345; // 0.75/0.75
+    unsigned int ageInSeconds = 4354654657; // 0.75/0.75
+    // total 6.75/6.75
     return 0;
 }
+// great work 😎 9/10 ⭐
