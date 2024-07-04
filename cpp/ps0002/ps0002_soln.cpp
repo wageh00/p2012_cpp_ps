5013e033015ddc930c9b6291567308a46d2c3a93
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-04 21:46:02 +0300

    problem 2 marked

---
+++ cpp/ps0002/ps0002_soln.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+int main()
+{
+	unsigned short number_of_people = 100; // 0.75/0.75
+	unsigned short number_of_medicine = 3; // 0.75/0.75
+	unsigned short number_of_team = 10; // 0.75/0.75
+	float price_of_one_cookie = 20; // 0.5/0.75
+	char first_letter = 'H'; // 0.75/0.75
+	unsigned int number_of_babies = 2874673562; // 0.5/0/75
+	long double seven_Division_3 = 2.3333333; // 0.75/0.75
+	long electricity_bill = 140826; // 0.5/0.75
+	unsigned int age_in_sec = 2000000000; // 0.25/0.75
+  // total 5.5/6.75
+  return 0;
+}
+// good work 👍 7.5/10
