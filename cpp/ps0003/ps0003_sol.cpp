c6080d9a4f40ca17da6a0590484c59796aad9b9d
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-05 12:20:38 +0300

    problem 3 marked

--- cpp/ps0003/ps0003_sol.cpp
+++ cpp/ps0003/ps0003_sol.cpp
@@ -2,12 +2,21 @@
 
 int main()
 {
+    // prompting user for number 2/2
     int x;
     std::cin >> x;
-    if(x%2==0)
-        std::cout << "Number is even";
-        else
-        std::cout << "Number is odd";
 
+    // checking if the number is odd or even 3/3
+    std::cout << "Number is ";
+    if(x % 2 == 0)
+        std::cout << "even";
+    else
+        std::cout << "odd";
+    std::cout << std::endl;
+    
     return 0;
 }
+// great work 😎 9/10
+
+// Note: u can use ternary operator
+// std::cout << "Number is " << ((x % 2 == 0) ? "even" : "odd") << std::endl
