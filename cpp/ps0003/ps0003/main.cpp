16cc52b4ea7ef4de2a5f3470e6ee8da37113cbbd
Mennatalahtaha <167816045+Mennatalahtaha@users.noreply.github.com>
2024-07-06 21:31:15 +0300

    Add files via upload

---
+++ cpp/ps0003/ps0003/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  unsigned short number;
+  cin>>number;
+  if(number%2==0)
+    cout<<"Number is even\n";
+  else
+     cout<<"Number is odd\n";
+    return 0;
+}
