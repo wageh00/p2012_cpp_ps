f53ce658e4889a0698118e8f092d572d820c86d9
Basmalah Osama <basmalahosama11@gmail.com>
2024-07-06 23:01:56 +0300

    solve of problem 4 . 2

--- cpp/ps0003/ps0003_sol.cpp
+++ cpp/ps0003/ps0003_sol.cpp
@@ -4,17 +4,8 @@ using namespace std;
 
 int main()
 {
-    short a ,b ,c ;
     short s ;
-    cin >> a ;
-    cout << "+\n" ;
-    cin >> b  ;
-    cout <<"+\n" ;
-     cin >> c;
-     cout << "=\n" ;
-     s= a + b + c ;
-     cout<< s << endl  ;
-
+    cin >> s ;
      if ( s % 2 == 0 ) {
         cout << "Number is even \n" ;
      } else {
