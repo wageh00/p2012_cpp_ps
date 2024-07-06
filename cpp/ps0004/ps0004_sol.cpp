1926bac8d2ce7c5efdea871425c4880b26b708e6
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-06 08:44:29 +0300

    problem 4 marked

--- cpp/ps0004/ps0004_sol.cpp
+++ cpp/ps0004/ps0004_sol.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 int main()
 {
-    int x;
-    std::cout << "Enter a number from 32 to 126:";
-    std::cin >> x;
+    // prompting user for number 1/1.5
+    float x; // 0/0.5
+    std::cout << "Enter a number from 32 to 126: ";
+    std::cin >> x; // 1/1
 
+    // checking invalidaty of user's input 3/4
     if (x==0)
-        std::cout << "Invalid character/s or zero value!" << std::endl;
-    else if(!(x%1==0))
-        std::cout << "Invalid: a floating point number!" << std::endl;
-    else if(x>126 || x<32)
-        std::cout << "Invalid: number is out of the range!" << std::endl;
-        else if(!(x>32 && x<126))
-            std::cout << "Invalid character/s or zero value!" << std::endl;
-    else{
-        char y{x};
-        std::cout << "The character for " << x << " is " << y << "." << std::endl;
+    {
+        std::cout << "Invalid character/s or zero value!" << std::endl; // 1/1
+        return 1;
+    }
+    
+    if((int) x != x)
+    {
+        std::cout << "Invalid: a floating point number!" << std::endl; // 0/1
+        return 2;
+    }
+    
+    if(x>126 || x<32)
+    {
+        std::cout << "Invalid: number is out of the range!" << std::endl; // 1/1
+        return 3;
     }
 
+    // print the character 2/2
+    std::cout << "The character for " << x << " is " << (char) x /* 1/1 */ << "." << std::endl; // 1/1
     return 0;
 }
+// good work 👍 7.5/10
