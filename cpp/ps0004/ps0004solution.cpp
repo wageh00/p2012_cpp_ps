6bb4b504fb90886d34160e883668d83cf3bd4aa6
Sarah <149259734+SaraMaher5@users.noreply.github.com>
2024-07-06 17:58:34 +0300

    Add files via upload

---
+++ cpp/ps0004/ps0004solution.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter a number from 32 to 126: ";
+
+	int number=1;   
+	cin >> number;
+	char ch = number;
+	
+	if ((126 > number) && (number > 32))
+		cout << "The character for " << number << " is " << ch << endl;
+	else 
+		cout << "Invalid character/s or zero value!";
+	
+	return 0;
+}
+
