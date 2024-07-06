1be5b96bb1c91a6fdfde8ac464fecf5db78a4bbd
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-06 20:22:21 +0300

    problem 3 marked

--- cpp/ps0003/ps0003_soin_cpp.cpp
+++ cpp/ps0003/ps0003_soin_cpp.cpp
@@ -2,11 +2,15 @@
 #include<conio.h>
 using namespace std;
 int main(){
+	// prompting user for number 2/2
 	int number;
 	cin >> number;
+
+	// checking if the number is odd or even 3/3
 	if (number % 2 == 0)
-		cout << "Number is evan\n";
+		cout << "Number is even\n";
 	else
 		cout << "Number is odd\n";
 	_getch();
 }
+// great work 😎 9/10 ⭐
