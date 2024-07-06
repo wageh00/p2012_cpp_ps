9575d32f715dc9f307c68d6d9b08e25706eedbcd
Sarah <149259734+SaraMaher5@users.noreply.github.com>
2024-07-06 20:56:34 +0300

    Add files via upload

---
+++ cpp/ps0005/ps0005solution.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter the two operands : \n";
+    int op1, op2;
+    cin >> op1 >> op2;
+
+    cout << "Enter the operator + - * / %: \n ";
+    char ch;
+    cin >> ch;
+
+    switch (ch)
+    {
+    case '+':
+        cout << "(" << op1 << ")" << " + " << "(" << op2 << ")" << " = " << op1 + op2;
+        break;
+
+    case '-':
+        cout << "(" << op1 << ")" << " - " << "(" << op2 << ")" << " = " << op1 - op2;
+        break;
+
+    case '*':
+        cout <<"(" <<op1 <<")" <<" * " << "(" << op2 << ")" << " = " << op1 * op2;
+        break;
+
+    case '/':
+        if (op2 > 0)
+            cout << "(" << op1 << ")" << " / " << "(" << op2 << ")" << " = " << op1 / op2;
+        else
+            cout << "Math error : cannot divide by 0 \n";
+        break;
+
+    case '%':
+        cout <<"(" <<op1 <<")" << " % " <<"(" <<op2 <<")" << " = " << op1 % op2;
+        break;
+
+    default :
+        cout << "Invalid character for operator." <<endl;
+    }
+    
+    
+   
+    return 0;
+}
+
