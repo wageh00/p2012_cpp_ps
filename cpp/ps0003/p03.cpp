bb08b5b3d101d55c7ba948ee90a74a0800d4f5f6
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-06 20:52:37 +0300

    problem 3 marked

--- cpp/ps0003/p03.cpp
+++ cpp/ps0003/p03.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int main()
 {
-  int x=0;
-  cin>>x;
- if (x%2==0)
-    cout<< "Ever"<<endl;
- else
-     cout<< "Odd"<<endl;
+  // prompting user for number 2/2
+  int x = 0;
+  cin >> num;
 
+  // checking if the number is odd or even 3/3
+  cout << "Number is ";
+  if (num % 2 == 0)
+    cout << "even" << endl;
+  else
+    cout << "odd" << endl;
+  
+  return 0;
 }
+// great work 😎 8/10 ⭐
