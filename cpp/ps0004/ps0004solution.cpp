ec0666ce09a822353efbb2c9d0804a79c1042087
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-06 19:06:55 +0300

    problem 4 marked

--- cpp/ps0004/ps0004solution.cpp
+++ cpp/ps0004/ps0004solution.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main()
 {
-	// prompting user for number 1.5/1.5
+	// prompting user for number 1/1.5
 	cout << "Enter a number from 32 to 126: ";
 	float number;   // initial value for the number we will try the program on it
 	cin >> number; // entering the number
 
+	/*
 	// checking invalidaty of user's input 1.5/4
 	// printing the number and the opposite printable character 2/2
 	if ((126 >= number) && (number >= 32)) // the vaild range for entering the number we want to test it
@@ -16,7 +17,29 @@ int main()
 	}
 	else 
 		cout << "Invalid character/s or zero value!" <<endl; 
+	*/
+
+	// checking invalidaty of user's input 4
+	if (!number)
+	{
+	        cout << "Invalid character/s or zero value!\n";
+	        return 1;
+	}
 	
+	if ((int) number != number)
+	{
+	        cout << "Invalid: a floating point number!\n";
+	        return 2;
+	}
+	
+	if (number < 32 || number > 126)
+	{
+		cout << "Invalid: number is out of the range!\n";
+	        return 3;
+	}
+	
+	// printing the number and the opposite printable character 2
+	cout << "The character for " << number << " is " << (char) number << "." << endl;
 	return 0;
 }
-// good work 👍 6.5
+// good work 👍 6/10
