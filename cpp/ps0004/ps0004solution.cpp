41cbe2bdc6ad3b1e12a6fb355e2c825e0bc1a671
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-06 18:48:39 +0300

    problem 4 marked

--- cpp/ps0004/ps0004solution.cpp
+++ cpp/ps0004/ps0004solution.cpp
@@ -1,19 +1,22 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 int main()
 {
+	// prompting user for number 1.5/1.5
 	cout << "Enter a number from 32 to 126: ";
+	float number;   // initial value for the number we will try the program on it
+	cin >> number; // entering the number
 
-	int number=1;   // initial value for the number we will try the program on it
-	cin >> number; // intering the number
-	char ch = number; // putting the number value in a character value so we can print the number in its ascci code character value and test it in range or no
-	
-	if ((126 > number) && (number > 32)) // the vaild range for entering the number we want to test it
+	// checking invalidaty of user's input 1.5/4
+	// printing the number and the opposite printable character 2/2
+	if ((126 >= number) && (number >= 32)) // the vaild range for entering the number we want to test it
+	{	
+		char ch = number; // putting the number value in a character value so we can print the number in its ascci code character value and test it in range or no
 		cout << "The character for " << number << " is " << ch << endl; //printing the ascci code character value
+	}
 	else 
 		cout << "Invalid character/s or zero value!" <<endl; 
 	
 	return 0;
 }
-
+// good work 👍 6.5
