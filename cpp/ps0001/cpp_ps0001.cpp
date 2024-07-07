1c1c1ff5fd5f655d24808971222672c4a406b9ed
Mohamed Wael <hamo50994@gmail.com>
2024-07-07 10:55:14 +0300

    problem 1

--- cpp/ps0001/cpp_ps0001.cpp
+++ cpp/ps0001/cpp_ps0001.cpp
@@ -1,7 +1,27 @@
-/* help your brother solve summation problems
-    155
-    940
-   +168
-  --------
-   1263
-*/
+ #include <iostream>
+using namespace std;
+int main()
+{
+
+ int x ;
+  int y ;
+  int z ;
+  int A ;
+
+ cout << "please enter rectangle x \n";
+ cin >>x ;
+ 
+  cout << "please enter rectangle y \n";
+ cin >>y ; 
+ 
+ cout << "please enter rectangle z \n";
+ cin >>z ;
+
+ //processing
+  A = x+y+z;
+
+ // output
+cout <<A<< "\n";
+
+    return 0;
+}
