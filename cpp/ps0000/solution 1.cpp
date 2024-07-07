3d4266e8fc846fe6737400b83a1f2b444ada4f70
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-07 18:20:16 +0300

    problem 0 marked

--- cpp/ps0000/solution 1.cpp
+++ cpp/ps0000/solution 1.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream> // 2/2
 using namespace std;
-int main ()
-[
-cout <<"Hello,World!/n";
-return 0;
-]
+
+int main () // 0.5/1
+{
+  cout << "Hello, World!/n"; // 3.5/4
+  return 0; // 1/1
+}
+// great work 😎 9/10 ⭐
