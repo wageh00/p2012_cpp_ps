65c2c7f4cef9287143248723306c13a405194ee6
Mohamed Wael <hamo50994@gmail.com>
2024-07-07 11:54:23 +0300

    problem 2

---
+++ cpp/ps0002/main (2).cpp
@@ -0,0 +1,9 @@
+short Population = '20';
+short Medicine='5';
+short Group members  ='5';
+float the price = '1.5';
+char first_name = 'A';
+int children ='50000';
+
+float Electricity bill = '70.50';
+double Age of people ='100.123456789';
