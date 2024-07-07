45f1e005d3817d041cb58c6c2ac56fad2ed8d1bf
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-07 11:35:12 +0300

    problem 4 marked

--- cpp/ps0004/ps0004/main.cpp
+++ cpp/ps0004/ps0004/main.cpp
@@ -2,33 +2,45 @@
 
 using namespace std;
 
+int check_number(float n1);
 
- int  check_number(float n1);
 int main()
-{float  x;
-cout<<"Enter a number from 32 to 126:\n";
-cin>>x;
-if(x>=32&&x<=126){
-if(check_number(x)==1)
-    cout<<"The character for"<<x<<"is"<<(char)x<<"\n";
-else
-    cout<<"invalid: a floating point number.\n";}
-           else{
+{
+   // prompting user for number 1/1.5
+   float x;
+   cout<<"Enter a number from 32 to 126:\n";
+   cin>>x;
 
-if((x<32||x>126)&&x!=0){
-    if(check_number(x)==1)
- cout<<"invalid .number is out of the range\n";
-else
-  cout<<"invalid: a floating point number.\n";}
-  else
-cout<<"Invalid character/s or zero value!\n";}
-    return 0;
+   // checking invalidaty of user's input 4/4
+	  // printing the number and the opposite printable character 2/2
+   if(x>=32&&x<=126){
+   {
+        if(check_number(x)==1)
+           cout<<"The character for"<<x<<"is"<<(char)x<<"\n";
+        else
+           cout<<"invalid: a floating point number.\n";}
+   }
+   else
+   {
+        if((x<32||x>126)&&x!=0){
+        {
+           if(check_number(x)==1)
+               cout<<"invalid .number is out of the range\n";
+           else
+               cout<<"invalid: a floating point number.\n";}
+        }
+        else
+           cout<<"Invalid character/s or zero value!\n";}
+   }
+   
+   return 0;
 }
-int  check_number(float n1){
-   int x=(int)n1;
 
-    if(x==n1)
-        return 1;
-    else
-        return 0;
+int check_number(float n1){
+ int x=(int)n1;
+ if(x==n1)
+  return 1;
+ else
+  return 0;
 }
+// great work 😎 8/10 ⭐
