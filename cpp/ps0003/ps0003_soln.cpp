6fbf439b849423beb44470aa7ddb17348ce615be
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-08 14:03:21 +0300

    problem 3 marked

---
+++ cpp/ps0003/ps0003_soln.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  // prompting user for number 2/2
+  int number;
+  cout << "please enter number \n";
+  cin >> number;
+
+  // checking if the number is odd or even 3/3
+  if (number % 2 == 0)
+    cout << "this number is even \n";
+  else
+    cout << "this number is odd\n";
+  
+  return 0;
+}
+// great work 😎 8/10 ⭐
