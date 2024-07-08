dfabec67d5f03a025531e22ed86b47c0a57f5d76
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-08 20:32:16 +0300

    problem 5 marked

--- cpp/ps0005/150daysprogramming.cpp
+++ cpp/ps0005/150daysprogramming.cpp
@@ -1,11 +1,71 @@
-// 150daysprogramming.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-using namespace std;
-#include<string>
+#include <string>
 
-//bool isFloat(string num);
+using namespace std;
+
+bool isFloat(string num);
+
+int main()
+{
+    // prompting user for 2 operands and an operator 2/2
+    string st1, st2;
+    cout << "Enter the two operands: \n";
+    cin >> st1;
+    cin >> st2;
+    cout << "\n\n Enter the operator + - * / %: \n";
+    char op;
+    cin >> op;
+    cout << endl << endl;
+    float num1, num2;
+    num1 = stof(st1);
+    num2 = stof(st2);
+
+    // checking user inputs and processing 4/4
+    // printing the result if all data is valid 1/1
+    switch (op) {
+        case '+':
+            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 + num2;
+            break;
+    
+        case '-':
+            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 - num2;
+            break;
+        
+        case '*':
+            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 * num2;
+            break;
+        
+        case '/':
+            if (num1 == 0 && num2 == 0) {
+                cout << "Infinite number of solutions: (0 / 0) is Undefined Quantity.\n";
+                break;
+            }
+            else if (num2 == 0) {
+                cout << "Math Error: cannot divide by 0.";
+                break;
+            }
+            else {
+                cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 / num2;
+                break;
+            } // 1/1
+        
+        case '%':
+            if (isFloat(st1) || isFloat(st2)) {
+                cout << "cannot execute modulo operation for floating point numbers.\n";
+                break;
+            }
+            else {
+                cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << (int)num1 % (int)num2;
+                break;
+            } // 1/1
+            
+        default:
+            cout << "Invalid character for operator.\n"; // 1/1
+    } // 1/1
+
+    return 0;
+}
+// great work 😎 9.5/10 ⭐
 
 bool isFloat(string num) {
     bool isFloating = false;
@@ -17,144 +77,3 @@ bool isFloat(string num) {
     }
     return isFloating;
 }
-
-
-int main()
-{
- 
-   
-    string st1, st2;
-    cout << "Enter the two operands: \n";
-    cin >> st1;
-    cin >> st2;
-    cout << "\n\n Enter the operator + - * / %: \n";
-    char op;
-    cin >> op;
-    cout << endl << endl;
-    bool isSt1Float = isFloat(st1);
-    bool isSt2Float = isFloat(st2);
-
-
-
-
-
-    float num1, num2;
-    num1 = stof(st1);
-    num2 = stof(st2);
-
-
-
-    switch (op) {
-    case '+':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 + num2;
-        break;
-
-    case '-':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 - num2;
-        break;
-    case '*':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 * num2;
-        break;
-    case '/':
-        if (num1 == 0 && num2 == 0) {
-            cout << "Infinite number of solutions: (0 / 0) is Undefined Quantity.\n";
-            break;
-        }
-        else if (num2 == 0) {
-            cout << "Math Error: cannot divide by 0.";
-            break;
-
-        }
-        else {
-            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 / num2;
-            break;
-        }
-
-
-    case '%':
-        if (isSt1Float || isSt2Float) {
-            cout << "cannot execute modulo operation for floating point numbers.\n";
-            break;
-        }
-        else {
-            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << (int)num1 % (int)num2;
-            break;
-        }
-    default:
-        cout << "Invalid character for operator.\n";
-
-
-
-    }
-    
-    
-}
-
-void days1506() {
-    string st1, st2;
-    cout << "Enter the two operands: \n";
-    cin >> st1;
-    cin >> st2;
-    cout << "\n\n Enter the operator + - * / %: \n";
-    char op;
-    cin >> op;
-    cout << endl << endl;
-    bool isSt1Float = isFloat(st1);
-    bool isSt2Float = isFloat(st2);
-
-
-
-
-
-    float num1, num2;
-    num1 = stof(st1);
-    num2 = stof(st2);
-
-
-
-    switch (op) {
-    case '+':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 + num2;
-        break;
-
-    case '-':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 - num2;
-        break;
-    case '*':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 * num2;
-        break;
-    case '/':
-        if (num1 == 0 && num2 == 0) {
-            cout << "Infinite number of solutions: (0 / 0) is Undefined Quantity.\n";
-            break;
-        }
-        else if (num2 == 0) {
-            cout << "Math Error: cannot divide by 0.";
-            break;
-
-        }
-        else {
-            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 / num2;
-            break;
-        }
-
-
-    case '%':
-        if (isSt1Float || isSt2Float) {
-            cout << "cannot execute modulo operation for floating point numbers.\n";
-            break;
-        }
-        else {
-            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << (int)num1 % (int)num2;
-            break;
-        }
-    default:
-        cout << "Invalid character for operator.\n";
-
-
-
-    }
-}
-
-
-
