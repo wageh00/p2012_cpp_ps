24a35214787ea910859e736b5c9ff6239f2aaf65
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-08 11:31:21 +0300

    problem 1 marked

--- cpp/ps0001/ps0001_solv.cpp
+++ cpp/ps0001/ps0001_solv.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main()
 {
-    int n1,n2,n3,sum=0;
-    for (int i =1 ; i <=25 ; i++)
+    // declared 3 variables to store user inputs 0.5/1
+    unsigned short n1,n2,n3,sum=0;
+    
+    for (int i = 1; i <= 25; i++)
     {
+        // prompting user for 3 numbers to add 1/1
         cout << "Enter 3 numbers \n";
-        cin >> n1>>n2>>n3;
-        sum = n1+n2+n3;
-        cout <<"----------"<<"\n"<<sum <<"\n";
+        cin >> n1 >> n2 >> n3;
+
+        // outputting the result of the summation process 2/2
+        sum = n1 + n2 + n3;
+        cout << "----------\n" << sum << "\n";
     }
 }
+// great work 😎 9/10 ⭐
