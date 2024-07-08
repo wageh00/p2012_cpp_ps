04dfc4f0b35afd3c174d642151b20cc2685cf1b5
Asmaa Hamdy <150098637+A-sm-aa-Ha-md-y@users.noreply.github.com>
2024-07-08 06:20:40 +0200

    Solution to the problem 0

---
+++ cpp/ps0000/ps0000_solv.cpp
@@ -0,0 +1,48 @@
+@iinclude <"iostream.h">   // incorrect
+
+#include <iostream>       // correct
+
+/* First error  : To call any library, you must use #include not @iinclude .
+   Second error : The library used is placed directly inside these brackets < > Without quotation marks " " .
+   Third error  : The correct library is this iostream not iostream.h .
+*/
+
+using namespace std;     // Missing
+
+/*You forgot your command std to use print/output ,
+ Because you cannot use command cout without use iostream library and command std .
+ iostream library means : io --> input output & stream --> the name of library .
+*/
+
+
+int main(void)          // incorrect
+
+int main()             // correct
+
+/* First error  : If you used void inside main function you must change int --> to be --> void too.
+   Second error : It tells you that no value is returned for this program so the command return must be deleted .
+   Third error  : In this program you need to return a value so must delete void & put int .
+*/
+
+{
+    cout >> "Hello , world! /n"   // incorrect
+
+    cout << "Hello , world! \n"; // correct
+
+    /* First error  : The brackets used here are for the cin/input command, not the cout/output command.
+       Second error : You are writing the wrong slant direction, it should be from this / to this \ .
+       Third error  : You forgot the semicolon that is used to specify that this is the end of the requested command
+                      and so that it moves to the next command.
+   */
+
+
+    return 1;     // incorrect
+
+    return 0;    //  correct
+
+    /* First  : You are writing the wrong value because return 0 in the main function means that the program executed successfully
+                and return 1 in the main function means that the program does not execute successfully and there is some error.
+       Second : You can use return 1 in a user-defined function because returning 1 means that the user-defined function returns true
+                and returning 0 means that the user-defined function returns false in the case of user-defined function too .
+    */
+}
