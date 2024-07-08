ba445486c5017eb0cd0f504453cd075ae642df6e
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-08 20:11:53 +0300

    problem 4 marked

--- cpp/ps0004/150daysprogramming.cpp
+++ cpp/ps0004/150daysprogramming.cpp
@@ -1,25 +1,42 @@
-// 150daysprogramming.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
 using namespace std;
-#include<string>
 
-//bool isFloat(string num);
+bool isNumber(string s);
+bool isFloat(string num);
 
-bool isFloat(string num) {
-    bool isFloating = false;
-    for (char c : num) {
-        if (c == '.') {
-            isFloating = true;
-            break;
-        }
+int main()
+{
+    // prompting user for number 1.5/1.5
+    string input;
+    cout << "Enter a number from 32 to 126: ";
+    cin >> input;
+
+    // checking invalidaty of user's input 4/4
+    // printing the number and the opposite printable character 2/2
+    if (!isNumber(input) || input=="0") {
+        cout << "Invalid character/s or zero value! \n";
     }
-    return isFloating;
+    else {
+        if (isFloat(input)) {
+            cout << "Invalid: a floating point number! \n";
+        }
+        else {
+            int intNumber = stoi(input);
+            if (intNumber >= 32 && intNumber <= 126) {
+
+                cout << "The character for " << intNumber << " is " << char(intNumber) << ".";
+            }
+            else {
+                cout << "Invalid: number is out of the range! \n";
+            }
+        }
+    }    
 }
+// great work 😎 8.5/10 ⭐
 
-
-bool isNumber( string s)
+bool isNumber(string s)
 {
     for (char c : s) {
         if (s.length() > 1 && c == '-') {
@@ -35,120 +52,11 @@ bool isNumber( string s)
     return true;
 }
 
-
-
-int main()
-{
-    string input;
-    cout << "Enter a number from 32 to 126: ";
-    cin >> input;
-
-    bool isNum = isNumber(input);
-
-    if (!isNum || input=="0") {
-        cout << "Invalid character/s or zero value! \n";
-    }
-    else {
-        bool isFloating = isFloat(input);
-        if (isFloating) {
-            cout << "Invalid: a floating point number! \n";
-        }
-        else {
-            int intNumber = stoi(input);
-            if (intNumber >= 32 && intNumber <= 126) {
-
-                cout << "The character for " << intNumber << " is "<< char(intNumber) << " .";
-            }
-            else {
-                cout << "Invalid: number is out of the range! \n";
-            }
+bool isFloat(string num) {
+    for (char c : num) {
+        if (c == '.') {
+            return true;
         }
     }
-
-   
-    
-  
-    
-    
+    return false;
 }
-
-void days150ps0006() {
-    string st1, st2;
-    cout << "Enter the two operands: \n";
-    cin >> st1;
-    cin >> st2;
-    cout << "\n\n Enter the operator + - * / %: \n";
-    char op;
-    cin >> op;
-    cout << endl << endl;
-    bool isSt1Float = isFloat(st1);
-    bool isSt2Float = isFloat(st2);
-
-
-
-
-
-    float num1, num2;
-    num1 = stof(st1);
-    num2 = stof(st2);
-
-
-
-    switch (op) {
-    case '+':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 + num2;
-        break;
-
-    case '-':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 - num2;
-        break;
-    case '*':
-        cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 * num2;
-        break;
-    case '/':
-        if (num1 == 0 && num2 == 0) {
-            cout << "Infinite number of solutions: (0 / 0) is Undefined Quantity.\n";
-            break;
-        }
-        else if (num2 == 0) {
-            cout << "Math Error: cannot divide by 0.";
-            break;
-
-        }
-        else {
-            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << num1 / num2;
-            break;
-        }
-
-
-    case '%':
-        if (isSt1Float || isSt2Float) {
-            cout << "cannot execute modulo operation for floating point numbers.\n";
-            break;
-        }
-        else {
-            cout << "(" << num1 << ") " << op << " (" << num2 << ") " << " = " << (int)num1 % (int)num2;
-            break;
-        }
-    default:
-        cout << "Invalid character for operator.\n";
-
-
-
-    }
-}
-
-void days150ps003() {
-
-    int number;
-    cout << " enter the number: ";
-    cin >> number;
-    if (number % 2 == 0) {
-        cout << "Number is Even";
-    }
-    else
-        cout << "Number is Odd";
-}
-
-
-
