22354559ed7a944ccea92fcd600456d1bf6c786a
p003F <Malaksayed01022@gmail.com>
2024-07-08 18:14:27 +0300

    Solve the problem 3

---
+++ cpp/ps0003/problem 3 s.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;    
+cout <<"Enter any number\n";
+cin >> number;
+if(number % 2 ==0)
+cout << " number is even \n";
+else
+cout << " number is odd \n" << endl;
+
+return 0;
+}
