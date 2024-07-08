9fdd7124f248eca54d3bfc79364a8013ada22b91
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-08 21:07:44 +0300

    problem 4 marked

--- cpp/ps0004/ps0004_soin_cpp.cpp
+++ cpp/ps0004/ps0004_soin_cpp.cpp
@@ -2,17 +2,27 @@
 #include <conio.h>
 using namespace std;
 int main() {
-	int num;
+	// prompting user for number 1/1.5
+	float num;
 	cout << "Enter a number (ASCII value between 32 and 126) \n";
 	cin >> num;
 
-	if (num >= 32 && num <= 126) {
+	// checking invalidaty of user's input 1.5/4
+	// printing the number and the opposite printable character 1.5/2
+	if(!num) {
+		cout << "Invalid character/s or zero value!\n";
+	}
+	else if((int) num != num) {
+		cout << "Invalid: a floating point number!\n";
+	}
+	else if(num >= 32 && num <= 126) {
 		char ch = static_cast<char>(num);
-		cout << "Equivalent character \n" << ch << endl;
+		cout << "Equivalent character for the number " << num << " is " << ch << "." << endl;
 	}
 	else {
-		cout << "Invalid input. ASCII value must be between 32 and 126\n" << std::endl;
+		cout << "Invalid: number is out of the range!\n";
 	}
 
 	_getch();
 }
+// good work 👍 5/10
