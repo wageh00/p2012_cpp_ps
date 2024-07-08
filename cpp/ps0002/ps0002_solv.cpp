3e20cd0807995c9c0d75cf3ee101f96ccd6a8341
Asmaa Hamdy <150098637+A-sm-aa-Ha-md-y@users.noreply.github.com>
2024-07-08 06:24:16 +0200

    Solution to the problem 2

---
+++ cpp/ps0002/ps0002_solv.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int calc_neighbors =500;
+    int Alternative_medicines =1000;
+    float piece_bread= 12.5;
+    char first_letter='A';
+    unsigned long int calc_China_Births=1234567890;
+    int division=2;
+    long double calc_Electricity_production=12345.1;
+    unsigned long long int Age_seconds=11223344556677;
+    cout <<calc_neighbors<<"\n" <<Alternative_medicines<<"\n" <<piece_bread<<"\n" <<first_letter<<"\n"
+    <<calc_China_Births<<"\n" <<division<<"\n" <<calc_Electricity_production<<"\n" <<Age_seconds<<"\n" ;
+    return 0;
+}
