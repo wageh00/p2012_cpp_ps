d46921bfcdce8dc6719e80905f0812bf5eb87cb6
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-09 19:46:14 +0300

    problem 3 marked

--- cpp/ps0002/main.cpp
+++ cpp/ps0002/main.cpp
@@ -2,37 +2,17 @@
 using namespace std;
 
 int main() {
-
-    unsigned short buildingPopulation = 30;
-
-
-    int medicineAlternatives = 5;
-    int teamMembers = 10;
-    float bakeryItemPrice = 2.5;
-
-
-    char firstLetterOfName = 'A';
-
-    unsigned long long chinaBirths = 14000000;
-
-
-    double preciseDivisionResult = 10 / 7;
-
-    double productionLineElectricityBill = 10000.50;
-
-    long personAgeInSeconds = 31536000;
-
-    // ÿ»«⁄… «·„ €Ì—«  ·· Õﬁﬁ „‰ «·ﬁÌ„
-    cout << "Building Population: " << buildingPopulation ;
-    cout << "Medicine Alternatives: " << medicineAlternatives;
-    cout << "Team Members: " << teamMembers ;
-    cout << "Bakery Item Price: " << bakeryItemPrice ;
-    cout << "First Letter of Name: " << firstLetterOfName;
-    cout << "China Births: " << chinaBirths ;
-    cout << "Precise Division Result: " << preciseDivisionResult ;
-    cout << "Production Line Electricity Bill: " << productionLineElectricityBill ;
-    cout << "Person Age in Seconds: " << personAgeInSeconds ;
-
+    unsigned short buildingPopulation = 30; // 0.75/0.75
+    unsigned short medicineAlternatives = 5; // 0.5/0.75
+    unsigned short teamMembers = 10; // 0.75/0.75
+    float bakeryItemPrice = 2.5; // 0.75/0.75
+    char firstLetterOfName = 'A'; // 0.75/0.75
+    unsigned int chinaBirths = 14000000; // 0.5/0.75
+    double preciseDivisionResult = 10 / 7; // 0.75/0.75
+    double productionLineElectricityBill = 10000.50; // 0.75/0.75
+    unsigned int personAgeInSeconds = 31536000; // 0.75/0.75
+    // total 6.25/6.75
+    
     return 0;
 }
-
+// great work üòé 8.5/10 ‚≠ê
