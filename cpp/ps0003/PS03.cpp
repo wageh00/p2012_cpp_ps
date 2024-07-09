a53b895e5abad8288fccf6adcd275d2731d3d841
Mohamedfawzzy <mohoamedfawzy@gmail.com>
2024-07-09 09:37:48 +0300

    Add files via upload

---
+++ cpp/ps0003/PS03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int number;
+  cout<< "please enter number \n";
+  cin>>number;
+
+  if (number%2==0)
+    cout<< "this number is even \n";
+  else
+    cout<< "this number is odd";
+    return 0;
+
+}
