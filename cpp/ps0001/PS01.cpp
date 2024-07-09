da0341731936b0fee87113db69767121604057d9
Mohamedfawzzy <mohoamedfawzy@gmail.com>
2024-07-09 08:38:51 +0300

    Add files via upload

---
+++ cpp/ps0001/PS01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  unsigned short num1, num2 ,num3;
+  cout<< "please enter first number \n";
+  cin>>num1;
+  cout<< "please enter second number \n";
+  cin>>num2;
+  cout<< "please enter thired number \n";
+  cin>>num3;
+  cout<<"the result is\t" <<num1+num2+num3<<"\n";
+
+    return 0;
+
+}
