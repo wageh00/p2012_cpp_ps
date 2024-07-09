ba9ab53736cb016e31a1cb6ed7063d523de000b0
norhanesmail76 <norhanesmailmoawed@gmail.com>
2024-07-09 16:07:39 +0300

    Add files via upload

---
+++ cpp/ps0001/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    cout << "Enter the third number: ";
+    cin >> num3;
+
+    int sum = num1 + num2 + num3;
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
