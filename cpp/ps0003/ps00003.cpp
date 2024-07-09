f2ccc0606d0d43ec11b9b46e531950d31603d2a3
mohaelmtrshmwal7anaysahby <136931347+mohaelmtrshmwal7anaysahby@users.noreply.github.com>
2024-07-09 23:54:02 +0300

    Add files via upload

---
+++ cpp/ps0003/ps00003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+
+{ 
+    int num1;
+    cin >> num1;
+    if(num1%2==0)
+    {
+      cout <<"The Number Is Even";
+    }
+    else
+    {
+      cout<<"The Number Is Odd";
+    }
+  return 0;
+}
