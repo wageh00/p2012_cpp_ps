0aea596bef73e89e41411b15660913597444aa62
norhanesmail76 <norhanesmailmoawed@gmail.com>
2024-07-09 16:23:07 +0300

    Add files via upload

---
+++ cpp/ps0002/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    unsigned short buildingPopulation = 30;
+
+
+    int medicineAlternatives = 5;
+    int teamMembers = 10;
+    float bakeryItemPrice = 2.5;
+
+
+    char firstLetterOfName = 'A';
+
+    unsigned long long chinaBirths = 14000000;
+
+
+    double preciseDivisionResult = 10 / 7;
+
+    double productionLineElectricityBill = 10000.50;
+
+    long personAgeInSeconds = 31536000;
+
+    // ÿ»«⁄… «·„ €Ì—«  ·· Õﬁﬁ „‰ «·ﬁÌ„
+    cout << "Building Population: " << buildingPopulation ;
+    cout << "Medicine Alternatives: " << medicineAlternatives;
+    cout << "Team Members: " << teamMembers ;
+    cout << "Bakery Item Price: " << bakeryItemPrice ;
+    cout << "First Letter of Name: " << firstLetterOfName;
+    cout << "China Births: " << chinaBirths ;
+    cout << "Precise Division Result: " << preciseDivisionResult ;
+    cout << "Production Line Electricity Bill: " << productionLineElectricityBill ;
+    cout << "Person Age in Seconds: " << personAgeInSeconds ;
+
+    return 0;
+}
+
