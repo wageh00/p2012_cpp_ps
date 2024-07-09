4228549272a5363bc0dcbb26a4fe2a55228f43e7
mohaelmtrshmwal7anaysahby <136931347+mohaelmtrshmwal7anaysahby@users.noreply.github.com>
2024-07-09 23:15:20 +0300

    Add files via upload

---
+++ cpp/ps0001/ps00001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1;
+  int num2;
+  int num3;
+  cin >>num1;
+  cin >> num2;
+  cin >> num3;
+  cout<<"The Solution Is\n"<<num1+num2+num3;
+  return 0;
+}
