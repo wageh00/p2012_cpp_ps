b40ce0e72059fbefcaca49f18475ceda801bca96
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-09 08:58:13 +0300

    problem 0 marked

--- cpp/ps0000/cpp_ps00000.cpp
+++ cpp/ps0000/cpp_ps00000.cpp
@@ -1,9 +1,12 @@
 /*Use # And Don Not Use ""*/
-#include <iostream>
+#include <iostream> // 2/2
 using namespace std;
-int main()
+int main() // 1/1
 {
   cout << "Hello, World!\n";/*Your Are Forget ; */
+  // 4/4
+  
   /*0 Not 1*/
-  return 0;
+  return 0; // 1/1
 }
+// great work 😎 11/10 ⭐
