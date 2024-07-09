21ceed2822ff206179267c0ae3c369e4cdb13200
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-09 08:53:28 +0300

    problem 5 marked

--- cpp/ps0005/ps0005_solv.cpp
+++ cpp/ps0005/ps0005_solv.cpp
@@ -4,82 +4,75 @@ using namespace std;
 
 int main()
 {
-    int x,y,result;
-    char z;
+    // prompting user for 2 operands and an operator 1.5/2
+    float num1, num2, result;
+    char op;
 
+    cout << "Enter the two operands : \n";
+    cin >> num1 >> num2;
 
-    cout<<" Enter the two operands : \n ";
-    cin>>x>>y ;
+    cout << "Enter the two operator + - * / % : \n";
+    cin >> op;
 
-    cout<<" Enter the two operator = - * / % : \n ";
-    cin>> z ;
-
-    if(z == '+' || z == '-'  ||z == '*' || z == '/' || z == '%')
+    // checking user inputs and processing 3/4
+    // printing the result if all data is valid 1/1
+    if(op == '+' || op == '-'  || op == '*' || op == '/' || op == '%')
     {
-
-        if(z == '+' )
+        
+        if(op == '+')
         {
-          result = x+y;
-          cout<<" ( "<< x <<" ) " << z <<" ( "<< y <<" ) " <<" = "<< result <<" \n ";
+          result = num1 + num2;
+          cout << " ( " << num1 << " ) " << op << " ( " << num2 << " ) " << " = " << result << "\n";
         }
-
-
-        else if(z == '-')
+        
+        else if(op == '-')
         {
-          result = x-y;
-          cout<<" ( "<< x <<" ) " << z <<" ( "<< y <<" ) " <<" = "<< result <<" \n ";
+          result = num1 - num2;
+          cout << " ( " << num1 << " ) " << op << " ( " << num2 << " ) " << " = " << result << "\n";
         }
-
-          else if(z == '*')
+        
+        else if(op == '*')
         {
-          result = x*y;
-          cout<<" ( "<< x <<" ) " << z <<" ( "<< y <<" ) " <<" = "<< result <<" \n ";
+          result = num1 * num2;
+          cout << " ( " << num1 << " ) " << op << " ( " << num2 << " ) " << " = " << result << "\n";
         }
-
-
-          else if(z == '/')
+        
+        else if(op == '/')
         {
-             if(z == '/' && x!=0 && y==0)
+            if(num1 != 0 && num2 == 0)
             {
-              cout<<" Math Error : cannot divide by 0 .  \n ";
+              cout << "Math Error : cannot divide by 0.\n ";
             }
-
-             else if(z == '/' && x==0 && y==0)
+            else if(num1 == 0 && num2 == 0)
             {
-              cout<<" Infinite number of solutions : ( 0 / 0 ) is Undefined Quantity .  \n ";
-
+              cout << "Infinite number of solutions : ( 0 / 0 ) is Undefined Quantity.\n ";
             }
-
-              else
+            else
             {
-              result = x/y;
-              cout<<" ( "<< x <<" ) " << z <<" ( "<< y <<" ) " <<" = "<< result <<" \n ";
+              result = num1 / num2;
+              cout << " ( " << num1 << " ) " << op << " ( " << num2 << " ) " << " = " << result << "\n";
             }
-        }
-
-          else if(z == '%')
+        } // 1/1
+        
+        else if(op == '%')
         {
-              if(z == '%' && ( x== (float)x || y== float(y)))
+            if(num1 != (int)num1 || num2 != (int)num2)
             {
-              cout<<" Cannot execute modulo operation for floating point numbers . \n ";
+              cout << "Cannot execute modulo operation for floating point numbers.\n";
             }
-
-              else
+            else
             {
-              result = x%y;
-              cout<<" ( "<< x <<" ) " << z <<" ( "<< y <<" ) " <<" = "<< result <<" \n ";
+              result = (int)num1 % (int)num2;
+              cout << " ( " << num1 << " ) " << op << " ( " << num2 << " ) " << " = " << result << "\n";
             }
-
-        }
-
+        } // 0/1
     }
-
-     else if(z != '+' || z != '-'  ||z != '*' || z != '/' || z != '%')
+    else
     {
       cout<<" Invalid character for operator .  \n ";
-    }
-
-
-
+    } // 1/1
+    // 1/1
+    
     return 0;
 }
+// good work 👍 6.5/10
