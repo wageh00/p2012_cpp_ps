4c136447e29a225a1f4db5ce2540f615e46a4e1d
Mohamedfawzzy <mohoamedfawzy@gmail.com>
2024-07-09 09:40:00 +0300

    Add files via upload

---
+++ cpp/ps0002/PS02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int residents =50;
+  int medicine=20;
+  int Programing_Team=50;
+  float price=10.5;
+  char first letter=M;
+  double china_populaltion=120000000;
+  double Division_7=0.142
+  float Elec_invoice=25000
+  unsigned int age_by second=6480000
+
+    return 0;
+}
