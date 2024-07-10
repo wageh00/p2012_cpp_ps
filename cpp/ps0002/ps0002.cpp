156e73b87f761a32e1947a8a1aa2d98e666ff1fc
mohaelmtrshmwal7anaysahby <136931347+mohaelmtrshmwal7anaysahby@users.noreply.github.com>
2024-07-10 14:34:58 +0300

    Add files via upload

---
+++ cpp/ps0002/ps0002.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int peoples_in_apartment=8;
+    int medicnies = 5;
+    int peoples_in_company=10;
+    float bread =10.5;
+    char first_letter='A';
+    long long china=1204020040504320;
+    double number=10.0;
+    double electricity_bill=123455;
+    long long seconds=1234455983920;
+    cout << peoples_in_apartment << "\n";
+    cout << medicnies << "\n";
+    cout << peoples_in_company << "\n";
+    cout << bread << "\n";
+    cout << first_letter << "\n";
+    cout << china << "\n";
+    cout << number << "\n";
+    cout <<electricity_bill << "\n";
+    cout << seconds << "\n";
+    return 0;   
+}
