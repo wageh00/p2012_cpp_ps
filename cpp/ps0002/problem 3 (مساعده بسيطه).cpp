e0f6160b3e750200bbf55de1099b76bbf0438951
p003F <Malaksayed01022@gmail.com>
2024-07-11 01:12:25 +0300

    Add files via upload

---
+++ cpp/ps0002/problem 3 (مساعده بسيطه).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+unsigned Buildingpopulation='100';
+int Medicationalternatives='50';
+unsigned Numberofteammembers='300';
+short priceofonebaked='5';
+char firstletter ='M';
+double numberofbirths='20000';
+float outputof7/numb='3.5';
+double electricitybill='100500';
+unsigned age='7000000000';
+return 0;
+}
+
+
+    
