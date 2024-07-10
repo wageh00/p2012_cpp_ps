9d0d878afee2ebe4cc4e721b922633b7139a2f11
Mohamed Wael <hamo50994@gmail.com>
2024-07-10 13:55:11 +0300

    problem 4

---
+++ cpp/ps0003/main (3).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int x;
+    cout <<"enter your x \n";
+    cin >>x;
+    if (x>=0)
+    if (x % 2 == 0)
+    cout <<"even \n";
+    
+    else
+    cout <<"odd \n";
+return 0;
+}
