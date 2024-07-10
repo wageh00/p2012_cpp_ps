45b4f689e835db694650f78e45589344e9d4e570
p003F <Malaksayed01022@gmail.com>
2024-07-10 21:55:14 +0300

    واجب الحساب الجزء الاول

---
+++ cpp/ps0001/problem 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number1,number2,number3;
+cout << "Please Enter 3 numbers \n" << endl;
+cin >> number1>>number2>>number3;
+int sum=(number1+number2+number3);
+ cout <<" sum equal\n "<<sum<<" \n";
+return 0;
+}
