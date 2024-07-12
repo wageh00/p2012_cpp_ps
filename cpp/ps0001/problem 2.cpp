0519c128f81fed0e8a2c81845ba2923366bfbac3
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-12 09:46:00 +0300

    problem 2 marked

--- cpp/ps0001/problem 2.cpp
+++ cpp/ps0001/problem 2.cpp
@@ -1,11 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-int number1,number2,number3;
-cout << "Please Enter 3 numbers \n" << endl;
-cin >> number1>>number2>>number3;
-int sum=(number1+number2+number3);
- cout <<" sum equal\n "<<sum<<" \n";
-return 0;
+  // prompting user for 3 numbers to add 1/2
+  unsigned short number1, number2, number3;
+  cout << "Please Enter 3 numbers \n" << endl;
+  cin >> number1 >> number2 >> number3;
+
+  // outputting sum of the 3 numbers 3/3
+  unsigned short = (number1 + number2 + number3);
+  cout << " sum equal\n " << sum << " \n";
+  
+  return 0;
 }
+// great work 😎 8/10 ⭐
