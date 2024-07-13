b565265054d01905883dcf7b9c8943b196c81590
Mohamed Wael <hamo50994@gmail.com>
2024-07-13 10:37:54 +0300

    ps0005

---
+++ cpp/ps0005/main (5).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int A;
+    int B;
+    char C;
+    float D;
+    
+   cout<<"enter your A , B \n";
+    cin>>A>>B;
+    
+   cout<<"enter your operator + - * / % \n";
+    cin>>C;
+    
+   switch(C)
+    { 
+    case '+' : 
+     cout<<"D="<<A+B<<"\n";
+      break ;
+      
+    case '-' :
+     cout<<"D="<<A-B<<"\n";
+      break;
+      
+    case '*' :
+     cout<<"D="<<A*B<<"\n";
+      break;
+     
+    case '/' :
+     cout<<"D="<<A/B<<"\n";
+      break;
+      
+    case '%' :
+     cout<<"D="<<A%B<<"\n";
+      break;
+    } 
+      
+    return 0;
+}
