e743f34e66550a57b6ddd5e9e58262cf9e8388a9
Mohamed Ashraf <107517330+Zero028@users.noreply.github.com>
2024-07-13 03:10:00 +0300

    Add files via upload

---
+++ cpp/ps0007/ps0007_sol.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+const float PI = 3.141f;
+
+float dim[2] = {0, 0};
+
+float area(char shape, float dim[]) {
+    float ar = 0;
+
+    switch (shape) {
+        case 's':
+            ar = dim[0] * dim[0];
+            break;
+        case 'c':
+            ar = PI * dim[0] * dim[0];
+            break;
+        case 'r':
+            ar = dim[0] * dim[1];
+            break;
+        default:
+            cout << "Invalid shape!" << endl;
+            break;
+    }
+
+    return ar;
+}
+
+int main() {
+    char shape;
+    float ar;
+    cout << "Enter the shape you want to calculate the area for (c:circle/r:rectangle/s:square): ";
+    cin >> shape;
+
+    switch (shape) {
+        case 's': {
+            cout << "Enter the side length: ";
+            cin >> dim[0];
+            while (dim[0] <= 0) {
+                cout << "Invalid length! Enter a positive number: ";
+                cin >> dim[0];
+            }
+            ar = area(shape, dim);
+            cout << "The area of the square is: " << ar << endl;
+            break;
+        }
+        case 'c': {
+            cout << "Enter the radius: ";
+            cin >> dim[0];
+            while (dim[0] <= 0) {
+                cout << "Invalid radius! Enter a positive number: ";
+                cin >> dim[0];
+            }
+            ar = area(shape, dim);
+            cout << "The area of the circle is: " << ar << endl;
+            break;
+        }
+        case 'r': {
+            cout << "Enter the length: ";
+            cin >> dim[0];
+            while (dim[0] <= 0) {
+                cout << "Invalid length! Enter a positive number: ";
+                cin >> dim[0];
+            }
+            cout << "Enter the width: ";
+            cin >> dim[1];
+            while (dim[1] <= 0) {
+                cout << "Invalid width! Enter a positive number: ";
+                cin >> dim[1];
+            }
+            ar = area(shape, dim);
+            cout << "The area of the rectangle is: " << ar << endl;
+            break;
+        }
+        default:
+            cout << "Invalid shape!" << endl;
+            break;
+    }
+
+    return 0;
+}
