5bc805d5f8031d14189410aa7328f601292a691a
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-15 18:28:23 +0300

    problem 4 marked

--- cpp/ps0004/problem 5.cpp
+++ cpp/ps0004/problem 5.cpp
@@ -1,26 +1,34 @@
-# include <iostream>
+#include <iostream>
 using namespace std ;
 int main ()
 {
-float number; 
+    // prompting user for number 1.5/1.5
+    float number; 
     cout << "Enter a number (must be between 32 and 126): ";
     cin >> number;
-    if ( number - int (number) != 0)
-  {  // إذا كان المدخل غير صحيح(رقم عشري)
+
+    // checking invalidaty of user's input 4/4
+	// printing the number and the opposite printable character 2/2
+    if (number - int(number) != 0)
+    {  // إذا كان المدخل غير صحيح(رقم عشري)
         cout << "Invalid: a floating point number " << endl;
     }
     
-   else if (number == 0 || cin.fail() ){
+    else if (number == 0 || cin.fail() ){
         // إاذا كان المدخل غير صحيح(مثلا حرف أو صفر)
         cout << "Invalid: character or zero value" << endl;
-        }
-      else if ( number < 32 || number > 126 || number < 0)
-      {//إذا كان المدخل سالب او خارج الرينج
-      cout << "Invalid number is out of the range" << endl;   }
-       else {
+    }
+    
+    else if ( number < 32 || number > 126)
+    {//إذا كان المدخل سالب او خارج الرينج
+        cout << "Invalid number is out of the range" << endl;
+    }
+    
+    else {
         // طباعة الرمز المقابل في جدول ASCII
         cout << "The character for " << number << " is " << char(number) << endl;
     }
 
     return 0;
 }
+// great work 😎 10.5/10 ⭐
