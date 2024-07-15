7a0622aa9aa42d51377fcfc22fe1b7a70f44038d
Mohamed Ashraf <107517330+Zero028@users.noreply.github.com>
2024-07-15 22:17:03 +0300

    Add files via upload

--- cpp/ps0006/ps0006_sol.cpp
+++ cpp/ps0006/ps0006_sol.cpp
@@ -2,80 +2,41 @@
 
 using namespace std;
 
-void counter(int end, int start=0, int step=1);
+int counter(int end, int start=0, int step=1);
 
 int main()
 {
-    int end,start=0,step=1;
-    string value,value2;
 
-    cout << "Enter the number you'll stop at:";
-    cin >> end;
-
-    while(end<=0){
-        cout << "Invalid number, please enter a positive number:";
-        cin >> end;
-    }
-
-    cout << "Do you want to set the start or step value? (yes/no):";
-    cin >> value;
-
-    while(value!="yes" && value!="no"){
-        cout << "Invalid choice, please enter (yes/no):";
-        cin >> value;
-    }
-
-    if(value=="yes"){
-        cout << "Enter the number you'll start at:";
-        cin >> start;
-    }
-    else if(value=="no"){
-        cout << "The default value will be used for start (0) and for step (1)" << endl;
-    }
-
-
-     while(start<0){
-        cout << "Invalid number, please enter a positive number or zero:";
-        cin >> start;
-    }
-
-    if(value=="yes"){
-    cout << "Do you want to set the step value? (yes/no):";
-    cin >> value2;
-
-    while(value2 !="yes" && value2 !="no"){
-        cout << "Invalid choice, please enter (yes/no):";
-        cin >> value2;
-    }
-
-    if(value2=="yes"){
-        cout << "Enter the number you'll take as a single step:";
-        cin >> step;
-    }
-     else if(value2=="no"){
-        cout << "The default value will be used (0)" << endl;
-    }
-
-     while(step<=0){
-        cout << "Invalid number, please enter a positive number:";
-        cin >> step;
-    }
-    }
-    if(start==0 && step==1)
-        counter(end);
-    else if(step==1)
-        counter(end,start);
-    else
-        counter(end,start,step);
-
-
-
-    return 0;
 }
 
-void counter(int end, int start, int step){
-    for(int i=start;i<=end;i+=step){
-      cout << i << endl;
+int counter(int end, int start, int step){
+
+    int sum{0};
+
+    if(end>start)
+        sum=end-start;
+
+    else if(start>end)
+        sum=start-end;
+
+    if((end<start && step>=0) || sum%step!=0){
+        cout << "Error: invalid value of steps" << endl;
+        return -1;
     }
+    else if(end>start){
+    for(int i=start;i<=end;i+=step)
+      cout << i << endl;
+      return 0;
+    }
+    else if(start>end){
+        for(int i=start; i>=end; i+=step)
+            cout << i << endl;
+        return 0;
+    }
+    else{
+        cout << start << endl;
+        return 0;
+    }
+
 }
 
