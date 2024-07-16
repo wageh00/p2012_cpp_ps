17d8dbe6f68fefda1ed7ad70d3ec73ca02283838
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2024-07-16 12:23:56 +0300

    Task1 --done

--- cpp/ps0001/cpp_ps0001.cpp
+++ cpp/ps0001/cpp_ps0001.cpp
@@ -5,3 +5,20 @@
   --------
    1263
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x, y, z;
+    cout << "Please Enter The Num: \n";
+    cin >> x;
+    cout << "+";
+    cin >> y;
+    cout << "+";
+    cin >> z;
+    cout << "=" << x + y + z;
+
+    return 0;
+}
