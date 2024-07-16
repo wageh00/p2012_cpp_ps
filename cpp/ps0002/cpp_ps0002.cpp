1a3fec084f51b108c1fa27a67a4cc9b984e12cba
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2024-07-16 12:38:56 +0300

    Task2 -- done

--- cpp/ps0002/cpp_ps0002.cpp
+++ cpp/ps0002/cpp_ps0002.cpp
@@ -5,3 +5,20 @@
     - example:
         int family_members = 8;
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char homeNum = 30;
+    char medicineAlt = 7;
+    short teamNum = 370;
+    float breadPrice = 1.25;
+    char firstLetter = 'M';
+    int chinaPopulation = 452152335;
+    double sevenDivision = 1.21521552;
+    float ElectricCost = 15235.3;
+    long long int ageInSecond = 6220800000;
+
+    return 0;
+}
