e39bb957772e7a4aa3308648a70d6f96f8addd59
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2024-07-16 14:18:23 +0300

    Task6 --done

--- cpp/ps0006/cpp_ps0006.cpp
+++ cpp/ps0006/cpp_ps0006.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
+using namespace std;
 
 void special_counter(int end, int start, int step);
+void special_counter(int end, int start);
+void special_counter(int end);
 
 int main()
 {
     special_counter(20);
+    cout << "\n";
     special_counter(20, 10);
+    cout << "\n";
     special_counter(20, 10, 5);
+    cout << "\n";
     special_counter(10, 20, -5);
-    
-    // special_counter(20, 10, -5); // error
+    cout << "\n";
+    special_counter(20, 10, -5); 
 
     return 0;
+}
+
+void special_counter(int end){
+
+    for (int i = 0; i <= end; (end > 0 ? i++ : i--))
+    {
+        cout << i << "\t";
+    }
+}
+
+void special_counter(int end, int start){
+
+    for (int i = start; i <= end; (end > start ? i++ : i--))
+    {
+        cout << i << "\t";
+    }
+}
+
+void special_counter(int end, int start, int step){
+    // error handling
+    if((end > start) && step < 0){
+        cout << "Error";
+        return;
+    }
+
+    if((end < start) && step > 0){
+        cout << "Error";
+        return;
+    }
+
+    // proccess
+    for (int i = start; i <= end; i += step)
+    {
+        cout << i << "\t";
+    }
 }
