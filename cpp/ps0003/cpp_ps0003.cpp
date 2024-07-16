73fe36898faf1ad33becb3e4ad0bdbfb3087f175
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2024-07-16 12:42:38 +0300

    Task3 -- done

--- cpp/ps0003/cpp_ps0003.cpp
+++ cpp/ps0003/cpp_ps0003.cpp
@@ -10,3 +10,19 @@
       => 20000
       <= Number is odd
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Please Enter The Number: \n";
+    cin >> x;
+
+    if(x%2 == 0)
+      cout << "Number is even";
+    else
+      cout << "Number is odd";
+
+    return 0;
+}
