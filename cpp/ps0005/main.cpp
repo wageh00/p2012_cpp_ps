dd8a5216aa7a797f4e13d98c09c942fb5ba4f908
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-17 17:27:15 +0300

    problem 5 marked

--- cpp/ps0005/main.cpp
+++ cpp/ps0005/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-
-using namespace std;
-
 #include <stdexcept>
 
 int main() {
+    // prompting user for 2 operands and an operator 2/2
     double num1, num2;
     char op;
-
-    // ﬁ—«¡… «·„œŒ·«  „‰ «·„” Œœ„
+    
     std::cout << "Enter first number: ";
     std::cin >> num1;
     std::cout << "Enter an operator (+, -, *, /, %): ";
@@ -16,8 +13,8 @@ int main() {
     std::cout << "Enter second number: ";
     std::cin >> num2;
 
+    // checking user inputs and processing 3/4
     try {
-        //  ‰›Ì– «·⁄„·Ì… «·Õ”«»Ì…
         double result;
         switch (op) {
             case '+':
@@ -32,16 +29,18 @@ int main() {
             case '/':
                 if (num2 == 0) throw std::runtime_error("Division by zero is not allowed.");
                 result = num1 / num2;
-                break;
+                break; // 0.5/1
             case '%':
-                if (static_cast<int>(num2) == 0) throw std::runtime_error("Division by zero is not allowed.");
-                result = static_cast<int>(num1) % static_cast<int>(num2);
-                break;
+                int test1 = static_cast<int>(num1), test2 = static_cast<int>(num2);
+                if (num2 == 0) throw std::runtime_error("Division by zero is not allowed.");
+                if (num1 == test1 && num2 == test2) result = test1 % test2;
+                else std::cout << "cannot execute modulo operation for floating point numbers." << std::endl;
+                break; // 0.5/1
             default:
-                throw std::invalid_argument("Invalid operator.");
-        }
+                throw std::invalid_argument("Invalid operator."); // 1/1
+        } // 1/1
 
-        // ⁄—÷ «·‰ ÌÃ…
+        // printing the result if all data is valid 1/1
         std::cout << "Result: " << result << std::endl;
     } catch (std::exception& e) {
         std::cout << "Error: " << e.what() << std::endl;
@@ -49,3 +48,4 @@ int main() {
 
     return 0;
 }
+// great work üòé 9/10 ‚≠ê
