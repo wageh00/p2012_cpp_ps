780fc827e894a23affafc9608053d687be264aff
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-17 22:35:50 +0300

    problem 7 marked

--- cpp/ps0007/main.cpp
+++ cpp/ps0007/main.cpp
@@ -1,98 +1,73 @@
 #include <iostream>
 
 using namespace std;
- const float pi=3.141f;
- float dimensions[2]={0,0};
- float area (char shape ,float dimensions[]);
+
+const float PI = 3.141f;
+float dimensions[2] = {0,0};
+
+float area (char shape, float dimensions[]);
+
 int main()
 {
-   char shape ;
-     cout<<"Enter shape (r)for rectangle, (s) for square ,(c) for circle"<<endl;
-     cin>>shape;
-
-
-
-
-
+   // prompting user for the shape 1.5/1.5
+   char shape;
+   cout << "Enter shape (r) for rectangle, (s) for square, (c) for circle" << endl;
+   cin >> shape;
+   
+   // check the validaty of the choice for the shape 1/1
+   // fill in the dimensions array 3/3
+   // printing out the area of the shape 1/1
    switch(shape){
-   case ('r'):
-       cout <<"Enter the length and the width"<<endl;
-       cin>>dimensions [0]>>dimensions [1];
-        if (dimensions[0]<=0|| (shape =='r'&&dimensions[1]<=0)){
-   cout<<"Dimensions must be positive numbers"<<endl;}
-   else
-
-     cout <<" the area is : "<<area (shape ,dimensions)<<endl;
-       break;
-
-
-
-
-   case ('s'):
-       cout <<"Enter  side length of the square "<<endl;
-       cin>>dimensions [0];
-        if (dimensions[0]<=0|| (shape =='r'&&dimensions[1]<=0)){
-   cout<<"Dimensions must be positive numbers"<<endl;}
-   else
-    cout <<" the area is : "<<area (shape ,dimensions)<<endl;
-
-       break;
-
-
-
-
-
-   case ('c'):
-       cout <<"Enter  radius for the circle "<<endl;
-       cin>>dimensions [0];
-        if (dimensions[0]<=0|| (shape =='r'&&dimensions[1]<=0)){
-   cout<<"Dimensions must be positive numbers"<<endl;}
-   else
-   cout <<" the area is : "<<area (shape ,dimensions)<<endl;
-       break;
-
-
-
-
-   default:
-    cout<<"invalid shape"<<endl;
-    break;
+       case 'r':
+           cout << "Enter the length and the width" << endl;
+           cin >> dimensions[0] >> dimensions [1];
+           if (dimensions[0] <= 0 || dimensions[1] <= 0){
+               cout << "Dimensions must be positive numbers" << endl;
+           }
+           else
+               cout << "the area is : " << area(shape, dimensions) << endl;
+           break;
+    
+       case 's':
+           cout << "Enter side length of the square " << endl;
+           cin >> dimensions[0];
+           if (dimensions[0] <= 0){
+               cout << "Dimensions must be positive numbers" << endl;
+           }
+           else
+               cout << "the area is : " << area(shape, dimensions) << endl;
+           break;
+    
+       case 'c':
+           cout << "Enter radius for the circle " << endl;
+           cin >> dimensions[0];
+           if (dimensions[0] <= 0){
+               cout << "Dimensions must be positive numbers" << endl;
+           }
+           else
+               cout << "the area is : " << area(shape, dimensions) << endl;
+           break;
+    
+       default:
+           cout << "invalid shape" << endl;
    }
 
-
-
-
-
-
-
-
-
-    return 0;
+   return 0;
 }
-float area (char shape ,float dimensions[]){
+
+float area (char shape, float dimensions[]){
     switch(shape){
+        case 'r':
+            return dimensions[0] * dimensions[1];
 
-     case('r') :
-      return dimensions[0]*dimensions[1];
-      break;
+        case 's':
+            return dimensions[0] * dimensions[0];
 
+        case 'c':
+            return PI * dimensions[0] * dimensions[0];
 
-
-      case('s'):
-          return dimensions[0]*dimensions[0];
-          break;
-
-
-
-
-      case ('c'):
-            return  pi*dimensions[0]*dimensions[0];
-              break;
-
-
-
-      default :
-             return 0.0;
-             break;
-           }
+        default:
+            return 0.0;
+    }
 }
+// great work 👍 8/10
