e2b46638637aa8942b1e76e68e01b4eb98989839
Rana Tarek <ranatarek833@gmail.com>
2024-07-18 14:51:45 +0300

    Problem 2 in C++

---
+++ cpp/ps0002/Task2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short int building_members = 15;
+    short int num_alternatives_to_medicines = 3;
+    int company_members = 125;
+    short int bakery_prices = 5;
+    char first_letter_in_the_name = 'A';
+    double num_of_births = 1784596;
+    float num div 7 = 1.3333;
+    double num_electricity_company_bills_in_egypt = 52379641;
+    unsigned int age_yuang_and_old_people = 4200000;
+}
