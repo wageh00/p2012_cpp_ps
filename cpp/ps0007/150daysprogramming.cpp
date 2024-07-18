d9e84c2724cbb4f0ecf06daea58e4273553260db
HajarSaeed <134366978+HajarSaeed@users.noreply.github.com>
2024-07-18 17:27:04 +0300

    Add files via upload

--- cpp/ps0007/150daysprogramming.cpp
+++ cpp/ps0007/150daysprogramming.cpp
@@ -18,14 +18,98 @@ void special_counter(int , int);
 void special_counter(int, int, int);
 
 
+const float PI = 3.141f;
+
+float dimensions[2] = { 0, 0 };
+
+float area(char shape, float dimensions[]);
+
 int main()
 {
 
- 
-   /// special_counter(-3);
-   // special_counter(-20, -10);
-    //special_counter(20, 10, 5);
-    //special_counter(10, 20, -5);
+    // prompting user for shape and dimensions 
+    char ch;
+    cout << "enter the shape char : \n" << "c for circle: \n r for rectangle: \n s for square: \n t for triangle: \n ";
+    cin >> ch;
+    cout << area(ch, dimensions);
+
+
+
+    return 0;
+
+}
+
+float area(char shape, float dimensions[]) {
+
+    float input1, input2;
+    switch (shape) {
+    case 'c':
+        // call circle area function
+        cout << " enter the circle radius: ";
+        cin >> input1;
+
+
+        if (input1 > 0) {
+            dimensions[0] = input1;
+
+          
+        }
+        return PI * dimensions[0]* dimensions[0];
+        break;
+    case 'r':
+        cout << " enter the rectangle length: ";
+        cin >> input1;
+
+        cout << " enter the rectangle width: ";
+        cin >> input2;
+
+
+        if (input1 > 0 && input2 > 0) {
+            dimensions[0] = input1;
+            dimensions[1] = input2;
+
+           
+        }
+        return  dimensions[0] * dimensions[1];
+        break;
+    case 's':
+        cout << " enter the square side length: ";
+        cin >> input1;
+
+
+        if (input1 > 0) {
+            dimensions[0] = input1;
+
+          
+        }
+        return  dimensions[0] * dimensions[0];
+        break;
+    case 't':
+        cout << " enter the triangle base length: ";
+        cin >> input1;
+
+        cout << " enter the triangle height length: ";
+        cin >> input2;
+
+
+        if (input1 > 0 && input2 > 0) {
+            dimensions[0] = input1;
+            dimensions[1] = input2;
+
+           
+        }
+        return  dimensions[0] * dimensions[1]/2;
+        break;
+    default:
+        cout << "wrong charcter ";
+    }
+
+
+
+
+
+
+
 
 }
 
