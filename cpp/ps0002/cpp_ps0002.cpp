96daf5f5282672721ed1e6ac6591dc57b86ce410
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-18 12:08:59 +0300

    problem 2 marked

--- cpp/ps0002/cpp_ps0002.cpp
+++ cpp/ps0002/cpp_ps0002.cpp
@@ -10,15 +10,16 @@
 using namespace std;
 
 int main(){
-    char homeNum = 30;
-    char medicineAlt = 7;
-    short teamNum = 370;
-    float breadPrice = 1.25;
-    char firstLetter = 'M';
-    int chinaPopulation = 452152335;
-    double sevenDivision = 1.21521552;
-    float ElectricCost = 15235.3;
-    long long int ageInSecond = 6220800000;
-
+    short homeNum = 30; // 0.5/0.75
+    short medicineAlt = 7; // 0.75/0.75
+    short teamNum = 370; // 0.75/0.75
+    float breadPrice = 1.25; // 0.75/0.75
+    char firstLetter = 'M'; // 0.75/0.75
+    int chinaPopulation = 452152335; // 0.75/0.75
+    double sevenDivision = 1.21521552; // 0.75/0.75
+    float ElectricCost = 15235.3; // 0.75/0.75
+    unsigned int ageInSecond = 3220800000; // 0.5/0.75
+    // total 6.25/6.75
     return 0;
 }
+// great work 😎 8.5/10 ⭐
