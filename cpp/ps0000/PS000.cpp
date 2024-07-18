75671ca4add1a06eb6efbaad122bd9bd3b1d649b
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-18 18:22:37 +0300

    problem 0 marked

--- cpp/ps0000/PS000.cpp
+++ cpp/ps0000/PS000.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+#include <iostream> // 2/2
 
 using namespace std;
 
-int main() // () not (void)
+int main() // 1/1
 {
-    cout << "Hello world!\n"; // don�t forget ;
-    return 0; // return 0 not 1
+    cout << "Hello, world!\n"; // don’t forget ; // 3/4
+    return 0; // return 0 not 1 // 1/1
 }
+// Great work 😎 9/10 ⭐
