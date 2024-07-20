22136223b9fd8ccd00dd604fd314b40209c3f008
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-21 02:23:12 +0300

    problem 7 marked

--- cpp/ps0007/150daysprogramming.cpp
+++ cpp/ps0007/150daysprogramming.cpp
@@ -1,22 +1,6 @@
-// 150daysprogramming.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
 using namespace std;
-#include<string>
-#include <cmath> 
-
-
-
-
-
-
-
-// void special_counter(int end, int start, int step);
-void special_counter(int);
-void special_counter(int , int);
-void special_counter(int, int, int);
-
 
 const float PI = 3.141f;
 
@@ -26,158 +10,64 @@ float area(char shape, float dimensions[]);
 
 int main()
 {
-
-    // prompting user for shape and dimensions 
+    // prompting user for shape and dimensions 1.5/1.5
     char ch;
-    cout << "enter the shape char : \n" << "c for circle: \n r for rectangle: \n s for square: \n t for triangle: \n ";
+    cout << "enter the shape char : \n c for circle: \n r for rectangle: \n s for square: \n t for triangle: \n ";
     cin >> ch;
-    cout << area(ch, dimensions);
 
-
-
+    // printing out the area of the shape 1/1
+    float shape_area = area(ch, dimensions);
+    if (shape_area == -1)
+    {
+        cout << "invalid shape or dimensions" << endl;
+        return 1;
+    }
+    cout << shape_area;
     return 0;
-
 }
 
 float area(char shape, float dimensions[]) {
-
-    float input1, input2;
+    // fill in the dimensions array 2/3
     switch (shape) {
-    case 'c':
-        // call circle area function
-        cout << " enter the circle radius: ";
-        cin >> input1;
-
-
-        if (input1 > 0) {
-            dimensions[0] = input1;
-
-          
-        }
-        return PI * dimensions[0]* dimensions[0];
-        break;
-    case 'r':
-        cout << " enter the rectangle length: ";
-        cin >> input1;
-
-        cout << " enter the rectangle width: ";
-        cin >> input2;
-
-
-        if (input1 > 0 && input2 > 0) {
-            dimensions[0] = input1;
-            dimensions[1] = input2;
-
-           
-        }
-        return  dimensions[0] * dimensions[1];
-        break;
-    case 's':
-        cout << " enter the square side length: ";
-        cin >> input1;
-
-
-        if (input1 > 0) {
-            dimensions[0] = input1;
-
-          
-        }
-        return  dimensions[0] * dimensions[0];
-        break;
-    case 't':
-        cout << " enter the triangle base length: ";
-        cin >> input1;
-
-        cout << " enter the triangle height length: ";
-        cin >> input2;
-
-
-        if (input1 > 0 && input2 > 0) {
-            dimensions[0] = input1;
-            dimensions[1] = input2;
-
-           
-        }
-        return  dimensions[0] * dimensions[1]/2;
-        break;
-    default:
-        cout << "wrong charcter ";
-    }
-
-
-
-
-
-
-
-
+        case 'c':
+            cout << " enter the circle radius: ";
+            cin >> dimensions[0];
+            if (dimensions[0] > 0) {
+                return PI * dimensions[0] * dimensions[0];
+            }
+            break;
+        
+        case 'r':
+            cout << " enter the rectangle length: ";
+            cin >> dimensions[0];
+            cout << " enter the rectangle width: ";
+            cin >> dimensions[1];
+            if (dimensions[0] > 0 && dimensions[1] > 0) {
+                return  dimensions[0] * dimensions[1];
+            }
+            break;
+        
+        case 's':
+            cout << " enter the square side length: ";
+            cin >> dimensions[0];
+            if (dimensions[0] > 0) {
+                return  dimensions[0] * dimensions[0];
+            }
+            break;
+        
+        case 't':
+            cout << " enter the triangle base length: ";
+            cin >> dimensions[0];
+            cout << " enter the triangle height length: ";
+            cin >> dimensions[1];
+            if (dimensions[0] > 0 && dimensions[1] > 0) {
+                return  dimensions[0] * dimensions[1] / 2;
+            }
+            break;
+        
+        default:
+            cout << "wrong charcter ";
+    } // dimensions must have positive value
+    return -1; // float return type causes error for invalid choice of shapes list so we have to use return
 }
-
-void special_counter(int x) {
-    if (x > 0) {
-        for (int i = 0; i <= x; i++) {
-            cout << i << "\t";
-        }
-    }
-    else
-    {
-        for (int i = 0; i >= x; i--) {
-            cout << i << "\t";
-        }
-
-    }
-    
-}
-
-void special_counter(int end , int start) {
-    if (end > start) {
-        for (int i = start; i <= end; i++) {
-            cout << i << "\t";
-        }
-    }
-    else {
-        for (int i = start; i >= end; i--) {
-            cout << i << "\t";
-        }
-       // cout << "start must be less than end";
-    }
-   
-
-}
-
-void special_counter(int end , int start , int step) {
-    if (end > start) {
-        for (int i = start; i <= end; i += step) {
-            cout << i << "\t";
-        }
-    }
-    else {
-        for (int i = start; i >= end; i+=step) {
-            cout << i << "\t";
-        }
-      //  cout << "start must be less than end";
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// good work 👍 7/10
