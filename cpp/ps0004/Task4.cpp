2ce1087cc395d65f9fa2cbf1479e2f77294a7e0b
Rana Tarek <ranatarek833@gmail.com>
2024-07-21 20:08:11 +0300

    Add files via upload
    Solution problem Task4

---
+++ cpp/ps0004/Task4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    int y;
+    char r;
+
+    cout << "Enter number from 32 to 126: \n" ;
+    cin >> x;
+
+    y=x;
+    r=x;
+
+    if ( x==0 )
+    {
+        cout << "Invalid character/s or zero value. \n";
+    }
+
+    else if ( y!=x )
+    {
+        cout << "Invalid: a floating point number. \n";
+    }
+
+    else if ( y==x )
+    {
+        if ( y<0 || y<32 || y>126 )
+        {
+            cout << "Invalid: number is out of the range. \n";
+        }
+
+        else
+        {
+            cout << "The character for " << y;
+            cout << " is " << r;
+
+        }
+    }
+
+    return 0;
+}
