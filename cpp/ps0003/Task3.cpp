2068bcff6a28f6e423d2f0270d067d961317ca8b
Rana Tarek <ranatarek833@gmail.com>
2024-07-21 20:07:02 +0300

    Add files via upload
    Solution of problem task3

---
+++ cpp/ps0003/Task3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+
+    cout << "Enter your number: \n";
+    cin >> x;
+
+    if (x % 2 == 0)
+    {
+    cout << "This number is even \n";
+    }
+    else
+    {
+    cout << "This number is odd \n";
+    }
+    return 0;
+}
