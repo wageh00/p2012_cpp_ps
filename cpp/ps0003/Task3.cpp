c5dfea5f508f26283d23399acd10732780473b3a
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-22 08:30:25 +0300

    problem 3 marked

--- cpp/ps0003/Task3.cpp
+++ cpp/ps0003/Task3.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 int main()
 {
+    // prompting user for number 2/2
     float x;
 
     cout << "Enter your number: \n";
     cin >> x;
 
+    // checking if the number is odd or even 3/3
     if (x % 2 == 0)
     {
-    cout << "This number is even \n";
+        cout << "This number is even \n";
     }
     else
     {
-    cout << "This number is odd \n";
+        cout << "This number is odd \n";
     }
     return 0;
 }
