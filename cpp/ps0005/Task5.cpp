36553c3f67ec8102efafed029b5183f50482ec2e
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-22 08:43:36 +0300

    problem 5 marked

--- cpp/ps0005/Task5.cpp
+++ cpp/ps0005/Task5.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    // prompting user for 2 operands and an operator 2/2
     float x, y ,z;
     int xx, yy;
     char r;
@@ -17,59 +18,62 @@ int main()
     cout << "Enter the operator / * - + % : \n";
     cin >> r;
 
-        switch (r)
-        {
-            case  '/' :
-                if(x != 0 && y == 0 )
-                {
-                    cout << "Math error: cannot divide by 0 \n";
-                }
-                else if (x == 0 && y == 0)
-                {
-                    cout << "Infinite number of solution= (0/0) undefined quantity. \n";
-                }
-                else
-                {
-                     z= x/y;
-                     cout << "(" << x << ") / "  "(" << y <<") =" << z <<"\n";
-                }
-                break;
+    // checking user inputs and processing 4/4
+    // printing the result if all data is valid 1/1
+    switch (r)
+    {
+        case  '/' :
+            if(x != 0 && y == 0 )
+            {
+                cout << "Math error: cannot divide by 0 \n";
+            }
+            else if (x == 0 && y == 0)
+            {
+                cout << "Infinite number of solution= (0/0) undefined quantity. \n";
+            }
+            else
+            {
+                 z= x/y;
+                 cout << "(" << x << ") / "  "(" << y <<") =" << z <<"\n";
+            }
+            break;
 
-            case '*':
-                 z= x*y;
-                 cout << "(" << x << ") * "  "(" << y <<") =" << z <<"\n";
-                 break;
+        case '*':
+             z= x*y;
+             cout << "(" << x << ") * "  "(" << y <<") =" << z <<"\n";
+             break;
 
-            case '+':
-                 z= x+y;
-                 cout << "(" << x << ") + "  "(" << y <<") =" << z <<"\n";
-                 break;
+        case '+':
+             z= x+y;
+             cout << "(" << x << ") + "  "(" << y <<") =" << z <<"\n";
+             break;
 
-            case '-':
-                 z= x-y;
-                 cout << "(" << x << ") - "  "(" << y <<") =" << z <<"\n";
-                 break;
+        case '-':
+             z= x-y;
+             cout << "(" << x << ") - "  "(" << y <<") =" << z <<"\n";
+             break;
 
 
-            case '%':
-                 xx= (int) x;
-                 yy= (int) y;
+        case '%':
+             xx= (int) x;
+             yy= (int) y;
 
-                 if (xx == x && yy == y)
-                 {
-                     z= xx % yy;
-                 cout << "(" << xx << ") % "  "(" << y <<") =" << z <<"\n";
-                 }
-                 else
-                 {
-                     cout << "cannot execute modulo operation for floating point numbers.";
-                 }
+             if (xx == x && yy == y)
+             {
+                 z= xx % yy;
+             cout << "(" << xx << ") % "  "(" << y <<") =" << z <<"\n";
+             }
+             else
+             {
+                 cout << "cannot execute modulo operation for floating point numbers.";
+             }
 
-                 break;
+             break;
 
-             default:
-                cout <<"Invalid character in operator \n";
-                break;
-        }
+         default:
+            cout <<"Invalid character in operator \n";
+            break;
+    }
 
 }
+// great work 😎 10/10 ⭐
