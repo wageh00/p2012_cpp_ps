ac9b589c80dee4a5dea04f96c098cda0ce30c1b4
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-25 15:39:02 +0300

    problem 1 marked

--- cpp/ps0001/cpp_ps0001.cpp
+++ cpp/ps0001/cpp_ps0001.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main()
 {
-    int n1,n2,n3,result=0;
+    // promptiung user for 3 numbers to add 1/2
+    short n1,n2,n3,result=0;
     cout<<"enter number 1 "<<endl;
-
     cin>>n1;
-cout<<"enter number 2 "<<endl;
-cin>>n2;
-cout<<"enter number 3 "<<endl;
-cin>>n3;
-result =n1+n2+n3;
-cout<<"sum = "<<result;
+    cout<<"enter number 2 "<<endl;
+    cin>>n2;
+    cout<<"enter number 3 "<<endl;
+    cin>>n3;
+
+    // outputting the result of the summation process 3/3
+    result = n1+n2+n3;
+    cout<<"sum = "<<result;
+    return 0;
 }
+// great work 😎 7/10 ⭐
