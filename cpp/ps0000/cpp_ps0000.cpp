04fe5670d910954b812a4c2e778ad0b3f8e0f75d
7ussen2del <helwaly75@gmail.com>
2024-07-25 14:35:56 +0300

    problem 0_in c++

--- cpp/ps0000/cpp_ps0000.cpp
+++ cpp/ps0000/cpp_ps0000.cpp
@@ -1,7 +1,7 @@
-@include <"iostream.h">
-
-int Main(void)
+#include<iostream>
+using namespace std;
+int main()
 {
-  cout >> "Hello, World!/n"
-  return 1;
+  cout<< "Hello, World!\n";
+  return 0;
 }
