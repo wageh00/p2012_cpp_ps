87166a023135986d74edb5a64e886951e0821560
7ussen2del <helwaly75@gmail.com>
2024-07-25 14:48:30 +0300

    problem 1

--- cpp/ps0001/cpp_ps0001.cpp
+++ cpp/ps0001/cpp_ps0001.cpp
@@ -1,7 +1,15 @@
-/* help your brother solve summation problems
-    155
-    940
-   +168
-  --------
-   1263
-*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,n3,result=0;
+    cout<<"enter number 1 "<<endl;
+
+    cin>>n1;
+cout<<"enter number 2 "<<endl;
+cin>>n2;
+cout<<"enter number 3 "<<endl;
+cin>>n3;
+result =n1+n2+n3;
+cout<<"sum = "<<result;
+}
