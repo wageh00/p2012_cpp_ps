c5b5cf08493bdddb27e74e6d3172ae29fdc2bc21
MonaAyman <hueproject6@gmail.com>
2024-07-28 22:38:36 +0300

    solution of problem 2

---
+++ cpp/ps0002/Problem 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int people=20;
+    int medicine=5;
+    int team=10;
+    float baked=12.5;
+    char name='M';
+    double born=9020000;
+    double divition=1.857;
+    float bill=1000.99;
+    double age=193.077;
+  return 0;
+
+}
