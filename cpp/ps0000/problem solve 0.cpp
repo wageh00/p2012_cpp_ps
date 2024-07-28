857648874dc9daef1c4a25e6a66d5e3a05dde82d
HamedMohamedZaki <hamedhamo2401@gmail.com>
2024-07-28 21:35:57 +0300

    problem solve 0

---
+++ cpp/ps0000/problem solve 0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// اول غلطه كتب @ وهي مفروض #
+// تانى غلطة في كلمه "iostream.h  هي مفروض من غير علامتين تنصيص ومن غير .h
+using namespace std;
+//نسي يكتب الجمله دي
+int main()
+// كتب كلمه void 
+// كتب Main بدل main 
+{
+  cout << "Hello World \n " ;
+// حط دي >> مكان دي <<
+// كتب /n بدل \n
+  return 0;
+// return0 مش return 1
+}
