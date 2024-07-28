62a16eb8fbe827b9b541513ef24d555fadff8282
MonaAyman <hueproject6@gmail.com>
2024-07-28 20:07:13 +0300

    solution of problem zero

---
+++ cpp/ps0000/problem 0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// first mistake : @ ,, # not #
+// second mistake : "iostream.h" ,, should be iostream without .h and ""
+using namespace std;
+//forgot this line
+int main()
+// he write void and we don't need it ,, Main instead main
+{
+  cout << "Hello World \n " ;
+// first mistake >> ,, << not >>
+// second mistake /n ,, \n not /n
+  return 0;
+// return 0 not return 1
+}
