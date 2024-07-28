905ac8a25549a4ef0e0adbe96713f00b841b2524
HamedMohamedZaki <hamedhamo2401@gmail.com>
2024-07-28 22:36:29 +0300

    problem solve 2

---
+++ cpp/ps0002/problem solve 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int people=18;
+    int medicine=7;
+    int team=10;
+    float baked=8.5;
+    char name='H';
+    double born=9020000;
+    double divition=2.428;
+    float bill=999.75;
+    double age=159.068;
+  return 0;
+
+}
