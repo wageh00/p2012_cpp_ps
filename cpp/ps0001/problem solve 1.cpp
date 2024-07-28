cf3245c654a7004002f77c0e94a64081d8f8de41
HamedMohamedZaki <hamedhamo2401@gmail.com>
2024-07-29 01:05:19 +0300

    problem solve 1

---
+++ cpp/ps0001/problem solve 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int first;
+    int second;
+    int third;
+    int sum;
+    cout <<"please enter first number\n";
+    cin >>first;
+    cout <<"please enter second number\n";
+    cin >>second;
+    cout <<"please enter third number\n";
+    cin >>third;
+    sum = (first + second + third);
+    cout <<"sum : \n";
+    cout << sum;
+
+  return 0;
+}
