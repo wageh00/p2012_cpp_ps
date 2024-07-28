5bf692e725cddcdf0a94ccf71582570ffa47975f
MonaAyman <hueproject6@gmail.com>
2024-07-28 21:13:59 +0300

    solution of problem one

---
+++ cpp/ps0001/problem 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    int num3;
+    int sum;
+    cout <<"please enter first number\n";
+    cin >>num1;
+    cout <<"please enter second number\n";
+    cin >>num2;
+    cout <<"please enter third number\n";
+    cin >>num3;
+    sum = (num1 + num2 + num3);
+    cout <<"sum : \n";
+    cout << sum;
+
+  return 0;
+
+}
