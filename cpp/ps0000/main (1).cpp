8fb9b3a2445129e1881a87f0a10d09db079dfb75
Asmaa372 <brjb89962@gmail.com>
2024-07-30 23:20:43 +0300

    Problem 0 in c++

---
+++ cpp/ps0000/main (1).cpp
@@ -0,0 +1,11 @@
+#inline <iostream>
+using namespace std;
+int main()
+{
+    :cout << "Hallo world";
+//@ cannot be used instead of # 
+// cannot be used int main(void)
+//cannot be used iostream.h
+//Not 1 but 0 
+return 0
+}
