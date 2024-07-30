be640012b6d4e40b6bea73ed90e6d987783cd38d
mohaelmtrshmwal7anaysahby <136931347+mohaelmtrshmwal7anaysahby@users.noreply.github.com>
2024-07-30 15:49:04 +0300

    Add files via upload

---
+++ cpp/ps0005/ps000sl-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+   int num1;
+   int num2;
+   string oper;
+   cin>>num1;
+   cin>>num2;
+   cin>>oper;
+   if (oper=="+")
+     cout<<num1+num2;
+   else if(oper=="-")
+     cout<<num1-num2;
+   else if (oper=="*")
+     cout<<num1*num2;
+   else if (oper=="/")
+     cout<<num1/num2;
+   else if (oper=="%")
+     cout<<num1%num2;
+   else
+      cout<<"invaild operation";
+   
+   return 0;
+}
