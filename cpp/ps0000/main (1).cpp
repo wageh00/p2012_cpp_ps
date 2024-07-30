558c73513e7a2434539be2b5995d0c6c16bdb986
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-30 23:28:26 +0300

    problem 0 marked

--- cpp/ps0000/main (1).cpp
+++ cpp/ps0000/main (1).cpp
@@ -1,11 +1,12 @@
-#inline <iostream>
+#include <iostream> // 2/2
 using namespace std;
-int main()
+int main() // 1/1
 {
-    :cout << "Hallo world";
-//@ cannot be used instead of # 
-// cannot be used int main(void)
-//cannot be used iostream.h
-//Not 1 but 0 
-return 0
+    cout << "Hello, World\n"; // 2/4
+    //@ cannot be used instead of # 
+    // cannot be used int main(void)
+    //cannot be used iostream.h
+    //Not 1 but 0 
+    return 0 // 1/1
 }
+// great work 😎 9/10 ⭐
