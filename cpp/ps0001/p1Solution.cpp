52aaf1cec0b04775cfc995870de2c8afb4952e9b
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-31 17:27:28 +0300

    Update p1Solution.cpp

--- cpp/ps0001/p1Solution.cpp
+++ cpp/ps0001/p1Solution.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-double CalculateSum(double num1, double num2, double num3) {
-	return num1 + num2 + num3;
-}
+
 int main() {
-
-	cout << "The sum of your 3 numbers is: " << CalculateSum(155, 940, 168);
-
-	/*
-	double num1, num2, num3, sum;
-	cout<< " Enter 3 number to sum:" ;
+	// promptiung user for 3 numbers to add 1/2
+	short num1, num2, num3, sum;
+	cout << "Enter 3 number to sum:" ;
 	cin >> num1 >> num2 >> num3;
+
+	// outputting the result of the summation process 3/3
 	sum = num1 + num2 + num3;
-	cout << "The of number is:" << sum;*/
+	cout << "The of number is:" << sum;
 
 	return 0;
-
 }
+// great work 😎 8/10 ⭐
