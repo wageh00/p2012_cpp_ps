bb672468353822e67dbeeb7fe0cb919142892fd0
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-31 17:48:17 +0300

    problem 3 marked

--- cpp/ps0003/p3Solution.cpp
+++ cpp/ps0003/p3Solution.cpp
@@ -9,11 +9,13 @@ void checkEvenorOdd(int num) {
 }
 
 int main() {
+	// prompting user for number 2/2
 	int num;
 	cout << "Enter a number to check if it's even or odd: ";
 	cin >> num;
+
+	// checking if the number is odd or even 3/3
 	checkEvenorOdd(num);
-
 	return 0;
-
 }
+// great work 😎 10/10 ⭐
