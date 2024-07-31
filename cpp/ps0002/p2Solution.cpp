71ead44e5acbc7dd32f9d7e824126f1704e64b70
Deena <deena.abd321@gmail.com>
2024-07-31 06:09:21 +0300

    week2_task2

---
+++ cpp/ps0002/p2Solution.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	int building_residents = 50;
+	int drug_substitutions = 3;
+	int team_members = 8;
+	float baked_unit_price = 5.5;
+	char first_letter_of_name = 'A';
+	long int Chinese_birth_number = pow(10, 6);
+	double division_by_seven = 3.14286;
+	//cout << 22.0 / 7;
+	float factory_electricity_bill = 22000.500;
+	long long age_in_seconds = pow(50, 10);
+	//cout << age_in_seconds;
+	return 0;
+
+}
