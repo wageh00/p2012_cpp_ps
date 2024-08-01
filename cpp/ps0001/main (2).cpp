c77f6e4650ccd7dbe4764986cc790197b990439b
Asmaa372 <brjb89962@gmail.com>
2024-08-01 23:32:05 +0300

    Problem 1 C++ Sp

---
+++ cpp/ps0001/main (2).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short Value1,Value2,value3;
+    cout <<"Please enter the three values you wish to add together";
+    cin>>Value1>>Value2>>value3;
+    cout <<Value1+Value2+value3;
+return 0;
+}
