6d9f2396e4a0139e9e780a0b802fa7b1c027cb84
Nour-Ahmed22 <nour32216@gmail.com>
2024-08-01 20:27:53 +0300

    problem 1

---
+++ cpp/ps0001_soin.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    int C;
+
+
+        cout << "Please enter the first Number A ? " << endl;
+        cin >> A; 
+
+        cout << "Please enter the first Number B ? " << endl;
+        cin >> B;
+
+        cout << "Please enter the first Number C ? " << endl;
+        cin >> C;
+
+        cout << "-----------------------------------------------" << endl;
+
+        cout << A + B + C << endl;
+
+
+
+
+    return 0;
+
+}
