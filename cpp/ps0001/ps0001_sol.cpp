2831552f1bc4f141d581c444128f2c4e50207208
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-01 10:37:22 +0300

    problem 1 marked

---
+++ cpp/ps0001/ps0001_sol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    // prompting user for 3 numbers to add 1.5/2
+    std::cout<<"Hello! please enter the numbers\n";
+    unsigned short a,b,c,sum;
+    std::cin>>a>>b>>c;
+    
+    // outputting the result of the summation process 3/3
+    sum=(a+b+c);
+    std::cout<<"the sum is: "<<sum<<"\n";
+
+    return 0;
+}
+// good work 👍 7.5/10
