17d746a236f2e7af4c090cbe86e964879d9601f2
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-02 23:02:48 +0300

    problem 5 marked

--- cpp/ps0005/problem 5.cpp
+++ cpp/ps0005/problem 5.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-int main() { 
+int main() {
+    // prompting user for 2 operands and an operator 2/2
     float num1, num2;
     char operation;
     cout << "Enter first number:\n";
@@ -10,42 +11,46 @@ int main() {
     cout << "Enter second number:\n";
     cin >> num2;
 
+    // checking user inputs and processing 4/4
     double result;
     switch(operation) {
         case '+':
-        result = num1 + num2;
-        break;
+            result = num1 + num2;
+            break;
+        
         case '-':
-        result = num1 - num2;
-        break;
+            result = num1 - num2;
+            break;
+        
         case '*':
-        result = num1 * num2;
-        break;
+            result = num1 * num2;
+            break;
+        
         case '/':
-        if (num2 != 0) {
-        result = num1 / num2;
-        }else {
-        cout << "Math eror cannot divide by zero \n" ;
-        return 1;
-        }
-        break;
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Math eror cannot divide by zero \n" ;
+                return 1;
+            }
+            break;
+        
         case '%':
-        if ((num1 == (int)num1) && (num2 == (int)num2)) {
-        result = (int)num1 % (int)num2;
-        }else {
-        cout << "Cannot execute modulo operation on floating numbers\n";
-        return 1;
-        }
-        break;
-        case '^':
-        cout << "invalid character for operator \n";
-        return 1;
-        break;
+            if ((num1 == (int)num1) && (num2 == (int)num2)) {
+            result = (int)num1 % (int)num2;
+            } else {
+                cout << "Cannot execute modulo operation on floating numbers\n";
+                return 1;
+            }
+            break;
+        
         default:
-        cout << "Invalid operation!\n";
-        return 1;
+            cout << "Invalid operation!\n";
+            return 1;
     }
 
+    // printing the result if all data is valid 1/1
     cout << "Result: " << result ;
     return 0;
 }
+// great work 😎 10/10 ⭐
