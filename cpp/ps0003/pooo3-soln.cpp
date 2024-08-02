7a3ea904dd093857c9e5df880cdf72d16c527db6
Menna-Ali56 <mennaalist@gmail.com>
2024-08-02 11:49:44 +0300

    problem 3

---
+++ cpp/ps0003/pooo3-soln.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+int main(){
+int number;
+cout<<"Enter your number:";
+cin>> number;
+if (number%2==0)
+{
+cout<<"the number "<<number<<" is even";
+}
+else 
+{
+  cout<<"the number "<<number<< "is odd" ;
+}
+
+return 0;
+}
