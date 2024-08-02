d5e337bb6a39b421fb2f3e7d30e3e250137621bb
Menna-Ali56 <mennaalist@gmail.com>
2024-08-02 07:41:38 +0300

    problem 1

---
+++ cpp/ps0001/p001-soln.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{ int number_one;
+  cin>>number_one;
+  int number_two;
+  cin>>number_two;
+  int number_three;
+  cin>>number_three;
+  int Summation=number_one+number_two+number_three;
+  cout<<"The sumation of three numbers ="<<Summation;
+  
+  return 0; 
+}
+
