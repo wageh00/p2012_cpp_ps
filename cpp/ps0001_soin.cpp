54b4e1a4caf670d9310b5ad0da7529457a9b91fe
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-02 18:26:50 +0300

    Update ps0001_soin.cpp

--- cpp/ps0001_soin.cpp
+++ cpp/ps0001_soin.cpp
@@ -5,27 +5,24 @@ using namespace std;
 
 int main()
 {
-    int A;
-    int B;
-    int C;
+    // prompting user for 3 numbers to add 1/2
+    short num1;
+    short num2;
+    short num3;
+    
+    cout << "Please enter the first Number A ? " << endl;
+    cin >> num1; 
 
+    cout << "Please enter the first Number B ? " << endl;
+    cin >> num2;
 
-        cout << "Please enter the first Number A ? " << endl;
-        cin >> A; 
+    cout << "Please enter the first Number C ? " << endl;
+    cin >> num3;
 
-        cout << "Please enter the first Number B ? " << endl;
-        cin >> B;
-
-        cout << "Please enter the first Number C ? " << endl;
-        cin >> C;
-
-        cout << "-----------------------------------------------" << endl;
-
-        cout << A + B + C << endl;
-
-
-
+    // outputting the result of the summation process 3/3
+    cout << "-----------------------------------------------" << endl;
+    cout << num1 + num2 + num3 << endl;
 
     return 0;
-
 }
+// great work 😎 10/10 ⭐
