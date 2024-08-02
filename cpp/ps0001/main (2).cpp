761110b3ae903aa3ff8ff829ff9a702c5f91839d
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-02 15:29:02 +0300

    problem 1 marked

--- cpp/ps0001/main (2).cpp
+++ cpp/ps0001/main (2).cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main()
 {
-    short Value1,Value2,value3;
-    cout <<"Please enter the three values you wish to add together";
-    cin>>Value1>>Value2>>value3;
-    cout <<Value1+Value2+value3;
-return 0;
+    // prompting user for 3 numbers to add 2/2
+    short value1, value2, value3;
+    cout << "Please enter the three values you wish to add together: \n";
+    cin >> value1 >> value2 >> value3;
+
+    // outputting the result of the summation process 3/3
+    cout << value1 + value2 + value3;
+    return 0;
 }
+// great work 😎 9/10 ⭐
