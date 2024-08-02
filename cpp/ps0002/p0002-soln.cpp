537f8e76cb5a6de52aa5168f277f8d9e1e7099d1
Menna-Ali56 <mennaalist@gmail.com>
2024-08-02 07:42:23 +0300

    problem 2

---
+++ cpp/ps0002/p0002-soln.cpp
@@ -0,0 +1,29 @@
+// عدد سكان عمارة
+int populationBuilding= 100;
+
+// عدد البدائل لدوا في الصيدلية
+int drug_Alternatives = 5;
+
+// أفراد تيم في شركة برمجة
+int team_Members = 8;
+
+// سعر واحدة من مخبوزات الفرن
+float bakery_Price = 3.44;
+
+// الحرف الأول من اسم شخص
+char first_char= 'A';
+
+// عدد المواليد في دولة تعدادها كبير زي الصين
+long long chinaBirths = 4985700000;
+
+// ناتج اكثر دقة لقسمة رقم على 7
+//تنفعfloat or double
+//بس double اكثر دقة
+double division_Result = 10.0 / 7.0;
+
+// فاتورة الكهرباء لخط انتاج في مصر
+//تنفعfloat or double
+double electricity_Bill = 1500.75;
+
+// عمر أشخاص كبار و صغار في السن بالثانية
+long long age_Seconds = 31536000; 
