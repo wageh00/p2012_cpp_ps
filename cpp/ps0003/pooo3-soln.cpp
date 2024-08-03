4f6e2003a03262ab3a6d2d665fa16f87a787c124
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-03 22:33:54 +0300

    problem 3 marked

--- cpp/ps0003/pooo3-soln.cpp
+++ cpp/ps0003/pooo3-soln.cpp
@@ -1,17 +1,23 @@
-# include <iostream>
-using namespace std;
-int main(){
-int number;
-cout<<"Enter your number:";
-cin>> number;
-if (number%2==0)
-{
-cout<<"the number "<<number<<" is even";
-}
-else 
-{
-  cout<<"the number "<<number<< "is odd" ;
-}
+#include <iostream>
 
-return 0;
+using namespace std;
+
+int main(){
+  // prompting user for number 2/2
+  int number;
+  cout << "Enter your number:";
+  cin >> number;
+  
+  // checking if the number is odd or even 3/3
+  if (number % 2 == 0)
+  {
+    cout << "the number " << number << " is even";
+  }
+  else 
+  {
+    cout << "the number " << number << " is odd";
+  }
+  
+  return 0;
 }
+// great work 😎 8/10 ⭐
