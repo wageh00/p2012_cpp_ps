988c4687006e4390c8ad358ef6a48d017a10d1b3
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-03 23:03:39 +0300

    problem 5 marked

--- cpp/ps0005/p0005.cpp
+++ cpp/ps0005/p0005.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    // prompting user for 2 operands and an operator 2/2
     char operation; // هو تقريبا الرموز الحسابيه char// مش متاكده 
     float num1, num2, result;
     
@@ -12,6 +13,8 @@ int main() {
     cin >> num1;
     cout << "Enter the second number: ";
     cin >> num2;
+
+    // checking user inputs and processing 3/4
     switch (operation) {
         case '+':
             result = num1 + num2;
@@ -27,24 +30,31 @@ int main() {
                 result = num1 / num2;
             } else {
                 cout << "Error: Division by zero" ;
-                return 1; // Exit the program
-            }
+                return 2; // Exit the program
+            } // 1/1
             break;
         case '%':
             if (int(num2)!= 0) {
-                result = int(num1) % int(num2);
+                if (int(num1) == num1 && int(num2) == num2))
+                    result = int(num1) % int(num2);
+                else
+                {
+                    cout << "cannot execute modulo operation for floating point numbers." << endl;
+                    return 4;
+                }
             } else {
                 cout << "Error: Division by zero" ;
-                return 1; 
-            }
+                return 2; 
+            } // 0.5/1
             break;
         default:
             cout << "Invalid operation!"  ;
-            return 1; 
-    }
+            return 1; // 1/1 
+    } // 0.5/1
 
-   
+    // printing the result if all data is valid 1/1
     cout << "Result: " << result << endl;
 
     return 0;
 }
+// great work 😎 8/10 ⭐
