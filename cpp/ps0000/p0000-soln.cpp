fffaa3bc5ad55c46e3ca23c63427fe398bd1207b
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-04 00:00:55 +0300

    problem 0 marked

--- cpp/ps0000/p0000-soln.cpp
+++ cpp/ps0000/p0000-soln.cpp
@@ -1,9 +1,7 @@
-#include <iostream> 
-
-using namespace std;
-
-int main(void) 
+#include <iostream> // 2/2
+int main(void) // 1/1
 {
-  cout << "Hello, World!\n"; 
-  return 0;
+  std::cout << "Hello, World!\n";  // 4/4
+  return 0; // 1/1
 }
+// great work 😎 10/10 ⭐
