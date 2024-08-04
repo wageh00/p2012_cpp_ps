daaf60e1bdb2fd2c2f0c1488248576c8bdc3dcb4
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-05 01:56:00 +0300

    problem 0 marked

--- cpp/ps0000/ps0000_sl.cpp
+++ cpp/ps0000/ps0000_sl.cpp
@@ -1,15 +1,16 @@
-#include <iostream> // importing libraries should be started with # not @ and name is iostream only
+#include <iostream> // importing libraries should be started with # not @ and name is iostream only 2/2
 
-// it's main not Main
+// it's main not Main 1/1
 int main() { 
 
     // it's << not >> we are printing data to the console not getting data
-    // add std::endl; to end the line and add a new line (break)
+    // add std::endl; to end the line and add a new line (break) 4/4
     std::cout << "Hello, World!" << std::endl;
 
     // same message with escape \n
     std::cout << "Hello, World!\n";
 
-    // main function runs your whole code return 0;
+    // main function runs your whole code return 0; 1/1
     return 0;
 }
+// great work 😎 11/10
