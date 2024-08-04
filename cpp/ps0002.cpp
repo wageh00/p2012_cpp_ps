8eb457e2872d8f4da2637d2984ed12bacd655263
Nour-Ahmed22 <nour32216@gmail.com>
2024-08-04 20:29:18 +0300

    problem 2 in c++

---
+++ cpp/ps0002.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short Population = 80;
+    short Alternatives = 4;
+    short Teammembers = 10;
+    float Theprice = 5.5;
+    char Thefirstletter = 'N';
+    int Thenumberofbirths = 9557867;
+    double Division = 1.7857142857142;
+    double Electricitybill = 10.99;
+    int unsigned Theage = 3973536000;
+
+
+    cout << "- The population of Amara  : " << Population << endl;
+
+    cout << "- Number of alternatives available at the pharmacy : " << Alternatives << endl;
+
+    cout << "- Team members at a programming company : " << Teammembers << endl;
+
+    cout << "- The price of one oven baked item : " << Theprice << endl;
+
+    cout << "- The first letter of a persons name : " << Thefirstletter << endl;
+
+    cout << "-The number of births in a country with a large popuiation like china : " << Thenumberofbirths << endl;
+
+    cout << "- A more accrate result for dividing 12.5/7 : " << Division << endl;
+
+    cout << "- Electricity bill for a production line in egypt : " << Electricitybill << endl;
+
+    cout << "- The Age of old and young people in seconds : " << Theage << endl;
+
+    return 0;
+
+}
