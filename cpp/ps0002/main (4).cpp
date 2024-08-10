4364896353494d7e6a60d46569ed5539fe149109
Amiramher797 <23546781az9@gmail.com>
2024-08-10 22:54:19 +0300

    problem2

---
+++ cpp/ps0002/main (4).cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Hello World";
+
+#include <iostream>
+	using namespace std;
+	int main()
+	{
+		int population_of_the_building =40;
+		int Alternative_Medicine = 4;
+		int Team_Mumbers_of_the_Programming_Company = 100;
+		float Price_of_a_loaf_of_bread = 5.5;
+		char The_Frist_Letter_of_a_persons_Name = 'S';
+		int Number_of_births = 134568;
+		float result = 3.2;
+		double deletectricity_bill_in_Egypt = 566899864212.456;
+		float Age = 245;
+		return 0;
+	}
