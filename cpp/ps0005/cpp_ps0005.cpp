aab28f614a44799aff42f390a2e2b10c5c4ad244
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-10 23:53:36 +0300

    problem 5 marked

--- cpp/ps0005/cpp_ps0005.cpp
+++ cpp/ps0005/cpp_ps0005.cpp
@@ -1,33 +1,10 @@
-/* ps0005 - very simple calculator.
-=========================================================
-Write a program that takes 2 numbers as operands and a character + - * / % as an operator and outputs the result.
-
-- If the inputted data is a valid, print
-      "(<PRINT_FIRST_OPERAND_HERE>) <PRINT_OPERATOR_HERE> (<PRINT_SECOND_OPERAND_HERE>) = <PRINT_RESULT_HERE>" and exit the program RETURNING 0.
-
-- If the inputted operator is not one of these + - * / %, print
-      "Invalid character for operator." and exit the program RETURNING 1.
-
-- If the inputted operator is division / and the second operand (divisor) is 0, print
-      "Math Error: cannot divide by 0." and exit the program RETURNING 2.
-
-- If the inputted operator is division / and the 2 operands are 0s, print
-      "Infinite number of solutions: (0 / 0) is Undefined Quantity." and exit the program RETURNING 3.
-
-- If the inputted operator is modulo % and at least one of the 2 operands is not integer, print
-      "cannot execute modulo operation for floating point numbers." and exit the program RETURNING 4.
-*/
-
 #include <iostream>
 #include <string>
 #include <cmath>
 
-
-
-
 int main()
 {
-      // prompting user for the 2 operands and an operator
+      // prompting user for the 2 operands and an operator 2/2
       double num1, num2, result;
       char op;
 
@@ -37,12 +14,7 @@ int main()
       std::cout << "Enter the operator + - * / % :" << std::endl;
       std::cin >> op;
 
-      // checking user inputs and processing------------
-      if(!num1 && !num2) {
-            std::cout << "Invalid character/s or zero valueInfinite number of solutions: (0 / 0) is Undefined Quantity!" << std::endl;
-            return 0;
-      }
-
+      // checking user inputs and processing------------ 3/4
       switch (op)
       {
       case '+':
@@ -58,30 +30,34 @@ int main()
             break;
 
       case '/':
-            if(!num2) {
+            if(num1 && !num2) {
                   std::cout << "Math Error: cannot divide by 0." << std::endl;
-                  return 0;
+                  return 2;
+            }
+            if(!num1 && !num2) {
+                  std::cout << "Invalid character/s or zero valueInfinite number of solutions: (0 / 0) is Undefined Quantity!" << std::endl;
+                  return 3;
             }
             result = num1 / num2;
             break;
 
       case '%':
-            if(std::floor(num1) != num1 || std::floor(num2) != num2) {
+            if(std::int(num1) != num1 || std::int(num2) != num2) {
                   std::cout << "cannot execute modulo operation for floating point numbers." << std::endl;
-                  return 0;
+                  return 4;
             }
             result = static_cast<int>(num1) % static_cast<int>(num2);
             break;
       
       default:
             std::cout << "Invalid character for operator." << std::endl;
-            return 0;
-            break;
+            return 1;
       }
 
-      // printing the result if all data is valid
+      // printing the result if all data is valid 1/1
       std::cout << "(" << num1 << ") " << op << " (" << num2 << ")" << " = " << result << std::endl;
 
 
       return 0;
 }
+// great work 😎 9/10 ⭐
