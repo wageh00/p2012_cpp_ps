23d2e3d136db23a1a8c16ad397c21b716810a2eb
Amiramher797 <23546781az9@gmail.com>
2024-08-10 18:51:08 +0300

    problem2

---
+++ cpp/ps0002/main (3).cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"please enter aNumber1?"<<endl;
+int MyNumber1;
+cin>>MyNumber;
+
+cout<<"please enter aNumber2?"<<endl;
+int MyNumber2;
+cin>>MyNumber2;
+
+cout<<"please enter aNumber3?"<<endl;
+int MyNumber3;
+cin>>MyNumber3;
+
+cout<<result;
+int myresult=MyNumber1+MyNumber2+MyNumber3;
+}
