f032af5c59bdcaae799f10bcd2b94210e75ef995
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-11 19:54:41 +0300

    problem 2 marked

---
+++ cpp/ps0002/ps0002_sol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short population = 100; // 0.5/0.75
+    short drug_alternatives = 9; // 0.5/0.75
+    short teamMembers = 5; // 0.75/0.75
+    float bakedPrice = 6.5; // 0.5/0.75
+    int birthsNumber = 5000000; // 0.5/0.75
+    char firstLetter = 'M'; // 0.75/0.75
+    double result = 5.45565612; // 0.25/0.75
+    float electricityBill = 100000000; // 0.75/0.75
+    unsigned int adultsAgechildrenAge = 1000000000; // 0.75/0.75
+    // total 5.25/6.75
+    
+    return 0;
+}
+// good work 👍 6.5/10
