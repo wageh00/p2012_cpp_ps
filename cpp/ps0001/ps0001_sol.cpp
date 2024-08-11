61039b4b6c2a5ae4d849c4cc50af964a4b60dbf2
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-11 19:39:10 +0300

    problem 1 marked

---
+++ cpp/ps0001/ps0001_sol.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // prompting user for 3 numbers to add 1/2
+    short num1, num2, num3;
+    cout << "please enter the first number" << endl;
+    cin >> num1;
+    cout << "please enter the second number" << endl;
+    cin >> num2;
+    cout << "please enter the third number" << endl;
+    cin >> num3;
+    
+    // calculating & outputting the result 3/3
+    short sum = num1 + num2 + num3;
+    cout << sum;
+    
+    return 0;
+}
+// good work 👍 7/10
