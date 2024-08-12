2451deb6e997da31bc77bbb194abfe3ed443e7e5
Abdelaziz-R <abdelazizroshdyy@gmail.com>
2024-08-12 16:20:43 +0300

    ps0001_Solution

---
+++ cpp/ps0001/ps0001_Solution.cpp
@@ -0,0 +1,32 @@
+/* help your brother solve summation problems
+    155
+    940
+   +168
+  --------
+   1263
+*/
+
+
+#include<iostream>
+
+int main(){
+
+int number1=155;
+int number2=940;
+int number3= 940;
+
+
+std::cout<<
+
+
+std::cout<< number1 <<endl;
+std::cout<< number2 <<endl;
+std::cout<< number13 <<endl;
+std::cout<<"--------"<<endl;
+std::cout<< number1+number2+number3 <<endl;
+
+
+
+    return 0;
+}
+
