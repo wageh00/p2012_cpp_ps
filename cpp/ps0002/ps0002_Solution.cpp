5f5472cdf461c32e8fc7be5911c2bf996c9ddb5c
Abdelaziz-R <abdelazizroshdyy@gmail.com>
2024-08-13 10:29:34 +0300

    Add files via upload

---
+++ cpp/ps0002/ps0002_Solution.cpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+class ps0002_Solution
+{
+
+    /* Give examples for variables from real life and consider that:
+    - variables should be named in a way that makes sense
+    - variables should has a suitable data type
+    - variables should be initialized with a suitable value
+    - example:
+        int family_members = 8{};
+*/
+
+
+
+
+    /*        
+    أي احسن نوع بيانات تختاره لمتغير هيشيل قيمة:
+- عدد سكان عمارة
+- عدد البدايل لدوا في الصيدلية
+- أفراد تيم في شركة برمجة
+- سعر واحدة من مخبوزات الفرن
+- الحرف الأول من اسم شخص
+- عدد المواليد في دولة تعدادها كبير زي الصين
+- ناتج اكثر دقة لقسمة رقم عىل 7
+- فاتورة الكهربا لخط انتاج في مصر
+- عمر أشخاص كبار و صغار ف السن بالثانية    
+    
+    */
+
+
+
+
+    int building_residents_count{40};
+    int medicine_alternatives{7};
+    int development_team_count{15};
+
+    float unti_price{15.50f};
+    char first_letter{'a'};
+    long int population_count{ 1344130000 };
+    float divide_result{ 3.5714285714285714285714285714286f };
+    float electricity_bill{150.45f};
+    long int age_in_seconds{ 1281657600 };
+
+
+
+
+
+
+}{};
+
