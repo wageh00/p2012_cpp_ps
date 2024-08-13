d56b7693b2f340eb6f846fd564e1664925e4de19
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-13 12:19:33 +0300

    problem 1 marked

--- cpp/ps0001/ps0001_Solution.cpp
+++ cpp/ps0001/ps0001_Solution.cpp
@@ -1,31 +1,19 @@
-/* help your brother solve summation problems
-    155
-    940
-   +168
-  --------
-   1263
-*/
-
-
 #include<iostream>
 
 int main() {
+    // prompting user for 3 numbers to add 1/2
+    short number1;
+    short number2;
+    short number3;
 
-    int number1;
-    int number2;
-    int number3;
-
-    
-   
     std::cin >> number1;
-    std::cin  >> number2;
-    std::cin  >> number3;
+    std::cin >> number2;
+    std::cin >> number3;
 
-
+    // outputting the result of the summation process 3/3
     std::cout << "--------" <<std:: endl;
     std::cout << number1 + number2 + number3 << std::endl;
 
-
-
     return 0;
 }
+// great work 👍 8/10 ⭐
