de63ab314eda56d676b66c81fcd6aaa1b3fd3404
Asmaa372 <brjb89962@gmail.com>
2024-08-15 17:43:38 +0300

    Problem 2 in c++

---
+++ cpp/ps0002/main (3).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int first=27;
+    int second=2;
+    int Team =10;
+     double better = 20.5;
+     char ch = 'A';
+
+    double age = 333.5;
+    short number = 1;
+    double Line = 444.4;
+    double Age =444.4 ;
+     return 0;
+}
