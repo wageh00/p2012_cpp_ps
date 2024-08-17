d6f612e53e6ff9a657eb9f6539eea914fdc012be
Youssef22878 <ibrahimalmahdi19752019@gmail.com>
2024-08-17 21:06:12 +0300

    Add files via upload

---
+++ cpp/ps0001/the second task.cpp
@@ -0,0 +1,26 @@
+
+
+
+#include <iostream>
+
+int main()
+
+{
+    int firstnumber , secondnuber , thirdnumber ;
+    
+    std::cout << "inter the first number\n";
+    std::cin >> firstnumber;
+
+    std::cout << "inter the second number\n";
+    std::cin >> secondnuber;
+
+    std::cout << "inter the thirdnumber number\n";
+    std::cin >> thirdnumber;
+    
+    int result = firstnumber + secondnuber + thirdnumber;
+    std::cout << result;
+
+    return 0;
+}
+
+
