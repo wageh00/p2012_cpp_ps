d8dd1ee3a523c81158423c87df507b696c3b9c4b
Youssef22878 <ibrahimalmahdi19752019@gmail.com>
2024-08-17 21:07:17 +0300

    Add files via upload

---
+++ cpp/ps0002/the third task.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    //عدد سكان عماره
+    short architecture = 30;
+   // عدد البدايل لدوا في الصيدليه 
+    int dragalternatives = 200;
+  //  افراد تيم فشركه برمجه
+    unsigned embloyees = 50;
+   // سعر واحده من مخبوزات الفرن
+     float bread = 5;
+    //الحرف الاول من اسم شخص 
+    char name ='Y';
+   // عدد المواليد
+    long birth = 1425114339;
+   // ناتج قسمه 12 على 7
+    float number=1.714;
+    //فاتوره الكهرباء 
+    float reseat = 12650;
+    //عمر رجل لديه 50 سنه من عمره بالثانيه 
+    unsigned age = 4320000;
+
+
+    return 0;
+}
+
