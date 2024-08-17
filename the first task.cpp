962fe2e8cd3fcdc85e5f1321d22f22ad9869ac31
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-17 21:07:30 +0300

    problem 0 marked

--- the first task.cpp
+++ the first task.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <iostream> // 2/2
 
-int Main()
+int main() // 0/1
 {
-	std::cout << "Hello, World!\n";
-		return 0;
+	std::cout << "Hello, World!\n"; // 4/4
+	return 0; // 1/1
 }
+
+// great work 😎 9/10 ⭐
