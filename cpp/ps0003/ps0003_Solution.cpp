bdece2057296da24d6427a47421cec88281557d2
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-18 07:30:34 +0300

    problem 3 marked

--- cpp/ps0003/ps0003_Solution.cpp
+++ cpp/ps0003/ps0003_Solution.cpp
@@ -1,40 +1,21 @@
-
-  /* help your brother check if the number is even or odd
-        - if the number is even, print "Number is even"
-        - if the number is odd, print "Number is odd"
-
-        - ex:
-          => 909
-          <= Number is odd
-
-        - another ex:
-          => 20000
-          <= Number is odd
-    */
-
-
 #include<iostream>
 
 int main() {
 
-  
-
-
-
+    // prompting user for number 2/2
     int number;
-
     std::cin >> number;
 
+    // checking if the number is odd or even 3/3
     if (number%2==0)
     {
         std::cout<<"Number is even"<< std::endl;
-
     }
     else
     {
         std::cout<<"Number is odd"<< std::endl;
     }
 
-
     return 0;
 }
+// great work 😎 8/10
