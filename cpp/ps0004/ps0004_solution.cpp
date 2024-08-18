da14a42c1297eabd831f320dcb2ecc6a6be628f9
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-18 07:38:58 +0300

    problem 4 marked

--- cpp/ps0004/ps0004_solution.cpp
+++ cpp/ps0004/ps0004_solution.cpp
@@ -2,39 +2,32 @@
 using namespace std;
 
 int main() 
-
 {
-    int input;
-
-
+    // prompting user for number 1/1.5
+    float input;
     cout << "Enter a number: ";
+    cin >> input;
 
-
-    if (!(cin >> input)) 
+    // checking invalidaty of user's input 2.5/4
+    if (input == 0) 
     {
         cout << "Invalid character/s or zero value!" << endl;
-            }
-
-    if (input >= 32 && input <= 126) 
-    {
-        cout << "The character for " << input << " is " << static_cast<char>(input) << "." << endl;
-
-    }
-
-    else if (input < 32 || input > 126) 
-
-    {
-        cout << "Invalid: number is out of the range!" << endl;
     }
 
     else if (static_cast<int>(input) != input) 
     {
         cout << "Invalid: a floating point number!" << endl;
     }
-
-    else 
+    
+    else if (input < 32 || input > 126) 
     {
-        cout << "Invalid character/s or zero value!" << endl;
+        cout << "Invalid: number is out of the range!" << endl;
     }
-
+    
+    else
+    {
+        // printing the number and the opposite printable character 2/2
+        cout << "The character for " << input << " is " << static_cast<char>(input) << "." << endl;
+    }
 }
+// good work 👍 6/10
