83d0a9fd7919fdd4b39a34c40e1733345e240e63
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-18 07:41:51 +0300

    Update ps0004_solution.cpp

--- cpp/ps0004/ps0004_solution.cpp
+++ cpp/ps0004/ps0004_solution.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "Enter a number: ";
     cin >> input;
 
-    // checking invalidaty of user's input 2.5/4
+    // checking invalidaty of user's input 3/4
     if (input == 0) 
     {
         cout << "Invalid character/s or zero value!" << endl;
@@ -30,4 +30,4 @@ int main()
         cout << "The character for " << input << " is " << static_cast<char>(input) << "." << endl;
     }
 }
-// good work 👍 6/10
+// good work 👍 6.5/10
