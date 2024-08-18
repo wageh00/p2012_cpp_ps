eb64258a4fe3dea741e0c09345788134e661a52b
Abdelrahmanhassan2009 <bdalrhmnmhmdhsan634@gmail.com>
2024-08-18 23:00:57 +0300

    Add files via upload

--- cpp/ps0003/main.cpp
+++ cpp/ps0003/main.cpp
@@ -1,37 +1,21 @@
+
 #include <iostream>
 using namespace std;
 
-int main() {
-    float num;
-    char ch;
+int main()
+ {
+   int number;
+cout<<"please enter a number "<<endl;
 
-    cout << "Enter a number between 32 and 126: ";
-    cin >> num;
-
-
-    if (num < 32 || num > 126) {
-        cout << "Invalid: number is out of the range." << endl;
-        return 1;
+    cin >> number;
+    if (number % 2 == 0)
+{
+        cout << "even" << endl;
     }
-
-
-    int test = num;
-    if (num != test) {
-        cout << "invalid: a floating point number" << endl;
-        return 1;
+else
+{
+        cout << "odd" <<endl;
     }
-
-
-    if (num == 0) {
-        cout << "Invalid character/s or zero value." << endl;
-        return 1;
-    }
-
-
-    ch = num;
-
-    cout << "The character for ASCII code " << num << " is: " << ch << endl;
 
     return 0;
-
 }
