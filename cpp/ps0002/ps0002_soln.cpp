e6743490ceeb10b7644589243b7235ddaf45580d
wageh00 <wagehwesam00@gmail.com>
2024-08-20 20:47:36 +0300

    Add files via upload
    problem 2

---
+++ cpp/ps0002/ps0002_soln.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned short int people = 28;//number 1
+
+    unsigned short int Alternative_medicine = 13 ;//number 2
+
+    unsigned short int member = 7;//number 3
+
+    float bread = 4.5;//number 4
+
+    char first_letter = 'w';//number 5
+
+    int Number_of_births = 9000000;//number 6
+
+    double divide_on_7 = 14.2857143;//number 7
+
+    float Electricity_bill = 1400000;//number 8
+
+    unsigned int age_in_scound = 10000000;//number 9
+
+    return 0;
+}
