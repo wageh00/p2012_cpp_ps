a0fc3baf89b5539bf9fa58435cef96a722f31a46
wageh00 <wagehwesam00@gmail.com>
2024-08-20 13:05:32 +0300

    problem 0001

---
+++ cpp/ps0001/ps0001_slon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cin >> a; "\n";
+    cin >> b; "\n";
+    cin >> c; "\n";
+    cout <<"============================\n";
+    cout <<"Answer is   ";
+    cout << a + b + c;
+
+    return 0;
+}
