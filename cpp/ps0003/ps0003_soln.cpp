eca5878e972d698f2cbf0336e9e4bffbce558130
wageh00 <wagehwesam00@gmail.com>
2024-08-24 23:08:13 +0300

    Add files via upload
    problem 3

---
+++ cpp/ps0003/ps0003_soln.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int age =17;
+int point =100;
+cout << (age>=18 && point >=100) <<  endl;//true
+cout << (age>=18 || point >= 100) << endl;//true
+    return 0;
+}
